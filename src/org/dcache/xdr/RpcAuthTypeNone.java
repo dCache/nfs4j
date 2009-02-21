66f7ab0457c9ce0bcb0d4b0d0beffb398b63d85b
Tigran Mkrtchyan <tigran.mkrtchyan@desy.de>
2009-02-21 22:53:47 +0100

    fixed ecnoding ot AuthNone

--- src/org/dcache/xdr/RpcAuthTypeNone.java
+++ src/org/dcache/xdr/RpcAuthTypeNone.java
@@ -6,13 +6,10 @@ public class RpcAuthTypeNone implements RpcAuth, XdrAble {
 
     private final int _type =  RpcAuthType.NONE;
 
-    private byte[] _data;
     private final static Logger _log = Logger.getLogger(RpcAuthTypeNone.class.getName());
 
     public void xdrDecode(XdrDecodingStream xdr) {
         int len = xdr.xdrDecodeInt();
-        _data = new byte[len];
-        xdr.xdrDecodeOpaque(_data, 0, len);
     }
 
     public int type() {
@@ -22,7 +19,7 @@ public class RpcAuthTypeNone implements RpcAuth, XdrAble {
     @Override
     public void xdrEncode(XdrEncodingStream xdr) throws OncRpcException {
        xdr.xdrEncodeInt(_type);
-       xdr.xdrEncodeOpaque(_data, 0, _data.length);
+       xdr.xdrEncodeInt(0);
     }
 
 }
