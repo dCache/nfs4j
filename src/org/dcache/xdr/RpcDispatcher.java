084e15cb5b2a9650c2f7fa88ba9a43a9fe5cb1dd
Tigran Mkrtchyan <tigran.mkrtchyan@desy.de>
2009-02-21 22:54:41 +0100

    runnig server framework.

--- src/org/dcache/xdr/RpcDispatcher.java
+++ src/org/dcache/xdr/RpcDispatcher.java
@@ -1,10 +1,23 @@
 package org.dcache.xdr;
 
+import java.io.File;
 import java.io.IOException;
+import java.net.InetAddress;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
+import org.acplt.oncrpc.OncRpcPortmapClient;
+import org.acplt.oncrpc.apps.jportmap.OncRpcEmbeddedPortmap;
+import org.dcache.chimera.FileSystemProvider;
+import org.dcache.chimera.JdbcFs;
+import org.dcache.chimera.XMLconfig;
+import org.dcache.chimera.nfs.ExportFile;
+import org.dcache.chimera.nfs.v3.HimeraNFSMountServerV2;
+import org.dcache.chimera.nfs.v3.HimeraNFSServerV2;
+import org.dcache.chimera.nfs.v4.DeviceManager;
+import org.dcache.chimera.nfs.v4.HimeraNFS4Server;
 
 import com.sun.grizzly.Context;
 import com.sun.grizzly.ProtocolFilter;
@@ -21,15 +34,34 @@ public class RpcDispatcher implements ProtocolFilter {
 
 
     public RpcDispatcher() {
-        _programs.put(100003, new RpcDispatchable() {
 
-            @Override
-            public void dispatchOncRpcCall(RpcCall call, XdrDecodingStream xdr) {
+        try {
+            XMLconfig config;
+            new OncRpcEmbeddedPortmap(2000);
 
-                call.reply(new RpcProgramVersionMismatch(2, 3));
-            }
+            OncRpcPortmapClient portmap =
+                new OncRpcPortmapClient(InetAddress.getByName("127.0.0.1"));
+            portmap.getOncRpcClient().setTimeout(2000);
+            portmap.setPort(100005, 3, 6, 2049);
+            portmap.setPort(100005, 1, 6, 2049);
+            portmap.setPort(100003, 4, 6, 2049);
+            config = new XMLconfig( new File("/home/tigran/eProjects/Chimera-hg/config.xml") );
+            FileSystemProvider fs = new JdbcFs( config );
+            ExportFile exports = new ExportFile( new File("/etc/exports"));
+            HimeraNFSMountServerV2 ms = new HimeraNFSMountServerV2(exports, fs);
+            HimeraNFSServerV2 nfs = new HimeraNFSServerV2(exports, fs);
 
-        });
+            HimeraNFS4Server nfs4 = new HimeraNFS4Server(new DeviceManager(), fs, exports);
+
+            _programs.put(100003, nfs4);
+            _log.log(Level.FINEST, " root inode: " + fs.path2inode("/pnfs") );
+            _programs.put(100005, ms);
+           // _programs.put(100003, nfs);
+
+        } catch (Exception e) {
+           _log.log(Level.SEVERE, "Failed to start Mount server:", e);
+        }
+
     }
 
     @Override
@@ -49,7 +81,7 @@ public class RpcDispatcher implements ProtocolFilter {
 
         RpcDispatchable program = _programs.get(Integer.valueOf(prog));
         if( program == null ) {
-            call.reply( new RpcProgUnavailable() );
+            call.failProgramUnavailable();
         }else{
             try {
                 program.dispatchOncRpcCall(call, xdr);
@@ -63,8 +95,7 @@ public class RpcDispatcher implements ProtocolFilter {
 
     @Override
     public boolean postExecute(Context context) throws IOException {
-        // TODO Auto-generated method stub
-        return false;
+        return true;
     }
 
 }
