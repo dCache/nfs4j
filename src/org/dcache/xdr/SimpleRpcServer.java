1a14d1dd56d7034459229a73fb46a25a7d465ff7
Tigran Mkrtchyan <tigran.mkrtchyan@desy.de>
2009-09-28 12:33:39 +0200

    imported patch simple-server.patch

--- src/org/dcache/xdr/SimpleRpcServer.java
+++ src/org/dcache/xdr/SimpleRpcServer.java
@@ -17,32 +17,16 @@
 
 package org.dcache.xdr;
 
-import com.sun.grizzly.BaseSelectionKeyHandler;
-import com.sun.grizzly.Controller;
-import com.sun.grizzly.DefaultProtocolChain;
-import com.sun.grizzly.DefaultProtocolChainInstanceHandler;
-import com.sun.grizzly.ProtocolChain;
-import com.sun.grizzly.ProtocolChainInstanceHandler;
-import com.sun.grizzly.ProtocolFilter;
-import com.sun.grizzly.TCPSelectorHandler;
-
-import com.sun.grizzly.util.DefaultThreadPool;
 import java.io.IOException;
 import java.net.InetAddress;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.concurrent.ExecutorService;
-import java.util.logging.Logger;
-import java.util.logging.Level;
 
 import org.acplt.oncrpc.OncRpcPortmapClient;
 import org.acplt.oncrpc.apps.jportmap.OncRpcEmbeddedPortmap;
 
 public class SimpleRpcServer {
 
-    private final static Logger _log = Logger.getLogger(SimpleRpcServer.class.getName());
-
     static final int DEFAULT_PORT = 1717;
+    private static final int PROG_NUMBER = 100017;
 
     public static void main(String[] args) throws Exception {
 
@@ -56,14 +40,12 @@ public class SimpleRpcServer {
             port = Integer.parseInt(args[0]);
         }
 
-        Map<Integer, RpcDispatchable> programs = new HashMap<Integer, RpcDispatchable>();
-
         new OncRpcEmbeddedPortmap(2000);
 
         OncRpcPortmapClient portmap = new OncRpcPortmapClient(InetAddress
                     .getByName("127.0.0.1"));
         portmap.getOncRpcClient().setTimeout(2000);
-        portmap.setPort(100017, 1, 6, port);
+        portmap.setPort(PROG_NUMBER, 1, 6, port);
 
         RpcDispatchable dummy = new RpcDispatchable() {
 
@@ -71,57 +53,11 @@ public class SimpleRpcServer {
                 call.reply(XdrVoid.XDR_VOID);
             }
 
-        };
-        programs.put(100003, dummy);
+        };        
 
-        final ProtocolFilter rpcFilter = new RpcParserProtocolFilter();
-        final ProtocolFilter rpcProcessor = new RpcProtocolFilter();
-        final ProtocolFilter rpcDispatcher = new RpcDispatcher(programs);
-
-        final Controller controller = new Controller();
-        final TCPSelectorHandler tcp_handler = new TCPSelectorHandler();
-        tcp_handler.setPort(port);
-        tcp_handler.setSelectionKeyHandler(new BaseSelectionKeyHandler());
-
-        controller.addSelectorHandler(tcp_handler);
-
-        DefaultThreadPool defp;
-        ExecutorService executorService = controller.getThreadPool();
-        if( executorService instanceof DefaultThreadPool ) {
-            defp = (DefaultThreadPool)executorService;
-        } else {
-            defp = new DefaultThreadPool();
-            controller.setThreadPool( defp );
-        }
-        defp.setInitialByteBufferSize( Xdr.MAX_XDR_SIZE );
-        controller.setReadThreadsCount(5);
-
-        final ProtocolChain protocolChain = new DefaultProtocolChain();
-        protocolChain.addFilter(rpcFilter);
-        protocolChain.addFilter(rpcProcessor);
-        protocolChain.addFilter(rpcDispatcher);
-
-        ((DefaultProtocolChain) protocolChain).setContinuousExecution(true);
-
-        ProtocolChainInstanceHandler pciHandler = new DefaultProtocolChainInstanceHandler() {
-            @Override
-            public ProtocolChain poll() {
-                return protocolChain;
-            }
-
-            @Override
-            public boolean offer(ProtocolChain pc) {
-                return false;
-            }
-        };
-
-        controller.setProtocolChainInstanceHandler(pciHandler);
-
-        try {
-            controller.start();
-        } catch (Exception e) {
-            _log.log(Level.SEVERE,"Exception in controller...", e);
-        }
+        OncRpcSvc service = new OncRpcSvc(port);
+        service.register(PROG_NUMBER, dummy);
+        service.start();
     }
 
 }
