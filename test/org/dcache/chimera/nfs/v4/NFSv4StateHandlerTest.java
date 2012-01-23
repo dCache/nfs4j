3a838add2eb420e8b4e65cb784581f98a2e1d187
Tigran Mkrtchyan <tigran.mkrtchyan@desy.de>
2012-01-23 10:13:24 +0100

    junit: test cleanup

--- test/org/dcache/chimera/nfs/v4/NFSv4StateHandlerTest.java
+++ test/org/dcache/chimera/nfs/v4/NFSv4StateHandlerTest.java
@@ -14,31 +14,29 @@ import org.dcache.chimera.nfs.ChimeraNFSException;
 public class NFSv4StateHandlerTest {
 
     private NFSv4StateHandler _stateHandler;
+    private NFS4Client _client;
 
     @Before
-    public void setUp() {
+    public void setUp() throws UnknownHostException {
         _stateHandler = new NFSv4StateHandler();
+        _client = createClient(_stateHandler);
     }
 
     @Test
     public void testGetByStateId() throws Exception {
-        NFS4Client client = createClient(_stateHandler);
-
-        stateid4 state = client.createState().stateid();
+        stateid4 state = _client.createState().stateid();
         _stateHandler.getClientIdByStateId(state);
     }
 
     @Test
     public void testGetByVerifier() throws Exception {
-        NFS4Client client = createClient(_stateHandler);
-
-        stateid4 state = client.createState().stateid();
-        assertEquals(client, _stateHandler.getClientByVerifier(client.verifier()));
+        stateid4 state = _client.createState().stateid();
+        assertEquals(_client, _stateHandler.getClientByVerifier(_client.verifier()));
     }
 
     @Test
     public void testGetByVerifierNotExists() throws Exception {
-        assertNull("get not exisintg", _stateHandler.getClientByVerifier( new verifier4()));
+        assertNull("get not existing", _stateHandler.getClientByVerifier( new verifier4()));
     }
 
     @Test(expected=ChimeraNFSException.class)
@@ -48,14 +46,13 @@ public class NFSv4StateHandlerTest {
 
     @Test
     public void testGetClientExists() throws Exception {
-         NFS4Client client = createClient(_stateHandler);
-        assertEquals(client,  _stateHandler.getClientByID(client.getId()));
+         _client = createClient(_stateHandler);
+        assertEquals(_client,  _stateHandler.getClientByID(_client.getId()));
     }
 
     @Test
     public void testUpdateLeaseTime() throws Exception {
-        NFS4Client client = createClient(_stateHandler);
-        NFS4State state = client.createState();
+        NFS4State state = _client.createState();
         stateid4 stateid = state.stateid();
         state.confirm();
         _stateHandler.updateClientLeaseTime(stateid);
@@ -63,8 +60,7 @@ public class NFSv4StateHandlerTest {
 
     @Test(expected=ChimeraNFSException.class)
     public void testUpdateLeaseTimeNotConfirmed() throws Exception {
-        NFS4Client client = createClient(_stateHandler);
-        NFS4State state = client.createState();
+        NFS4State state = _client.createState();
         stateid4 stateid = state.stateid();
 
         _stateHandler.updateClientLeaseTime(stateid);
@@ -72,13 +68,12 @@ public class NFSv4StateHandlerTest {
 
     @Test(expected=ChimeraNFSException.class)
     public void testUpdateLeaseTimeNotExists() throws Exception {
-        NFS4Client client = createClient(_stateHandler);
-        stateid4 state = client.createState().stateid();
+        stateid4 state = _client.createState().stateid();
         _stateHandler.updateClientLeaseTime(state);
     }
 
     static NFS4Client createClient(NFSv4StateHandler stateHandler) throws UnknownHostException {
-        InetSocketAddress address = new InetSocketAddress(InetAddress.getByName("www.google.com"), 123);
+        InetSocketAddress address = new InetSocketAddress(InetAddress.getByName(null), 123);
         return stateHandler.createClient(address, address, "123".getBytes(), new verifier4("123".getBytes()), null);
     }
 }
