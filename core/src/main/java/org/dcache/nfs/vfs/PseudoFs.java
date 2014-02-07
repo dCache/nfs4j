dCache/nfs4j

commit 1c916c5fb5476aac2e8445a30d92832e8b156caa

vfs: add a special option to hide dcache from dcap clients
applications built with libdcap can detect dCache through
mounted NFS and will switch to dcap. To enforce NFS access
hide dCache from them.

target: master, 0.7
Acked-by: Paul Millar

core/src/main/java/org/dcache/nfs/vfs/PseudoFs.java
@@ -154,6 +154,15 @@ public class PseudoFs implements VirtualFileSystem {
     @Override
     public Inode lookup(Inode parent, String path) throws IOException {
         checkAccess(parent, ACE4_EXECUTE);
+
+	/*
+	 * REVISIT: this is not ;the best place to do it, but the simples one.
+	 */
+	FsExport export = _exportFile.getExport(parent.exportIndex(), _inetAddress);
+	if (!export.isWithDcap() && ".(get)(cursor)".equals(path)) {
+	    throw new ChimeraNFSException(nfsstat.NFSERR_NOENT, "the dcap magic file is blocked");
+	}
+
         if (parent.isPesudoInode()) {
             return lookupInPseudoDirectory(parent, path);
         }

core/src/main/java/org/dcache/nfs/ExportFile.java
@@ -166,7 +166,17 @@ public class ExportFile {
                                 continue;
                             }
 
-                            throw new IllegalArgumentException("Unsupported option: " + option);
+			    if (option.equals("dcap")) {
+				exportBuilder.withDcap();
+				continue;
+			    }
+
+			    if (option.equals("no_dcap")) {
+				exportBuilder.withoutDcap();
+				continue;
+			    }
+
+			    throw new IllegalArgumentException("Unsupported option: " + option);
                         }
                         FsExport export = exportBuilder.build(path);
                         exports.add(export);

core/src/main/java/org/dcache/nfs/FsExport.java
@@ -58,6 +58,7 @@ public class FsExport {
     private final boolean _allSquash;
     private final int _anonUid;
     private final int _anonGid;
+    private final boolean _withDcap;
 
     /**
      * NFS clients may be specified in a number of ways:<br>
@@ -105,6 +106,7 @@ public class FsExport {
         _allSquash = builder.hasAllSquash();
         _anonUid = builder.getAnonUid();
         _anonGid = builder.getAnonGid();
+	_withDcap = builder.isWithDcap();
     }
 
     public String getPath() {
@@ -125,7 +127,9 @@ public class FsExport {
                 .append(',')
                 .append(_withAcl ? "acl" : "noacl")
                 .append(',')
-                .append("sec=").append(_sec);
+                .append("sec=").append(_sec)
+		.append(',')
+		.append(_withDcap ? "dcap" : "no_dcap");
         if (_allSquash) {
             sb.append(",all_squash");
         }
@@ -196,6 +200,10 @@ public class FsExport {
         return _anonGid;
     }
 
+    public boolean isWithDcap() {
+	return _withDcap;
+    }
+
     public static class FsExportBuilder {
 
         private String _client = "*";
@@ -206,6 +214,7 @@ public class FsExport {
         private boolean allSquash = false;
         private int _anonUid = DEFAULT_ANON_UID;
         private int _anonGid = DEFAULT_ANON_GID;
+	private boolean _withDcap = true;
 
         public FsExportBuilder forClient(String client) {
             _client = client;
@@ -262,6 +271,16 @@ public class FsExport {
             return this;
         }
 
+	public FsExportBuilder withDcap() {
+	    _withDcap = true;
+	    return this;
+	}
+
+	public FsExportBuilder withoutDcap() {
+	    _withDcap = false;
+	    return this;
+	}
+
         public String getClient() {
             return _client;
         }
@@ -293,6 +312,10 @@ public class FsExport {
         public int getAnonGid() {
             return _anonGid;
         }
+
+	public boolean isWithDcap() {
+	    return _withDcap;
+	}
 
         public FsExport build(String path) {
             return new FsExport(path, this);

core/src/test/java/org/dcache/nfs/FsExportTest.java
@@ -153,4 +153,17 @@ public class FsExportTest {
         assertEquals("default anonuid not used", FsExport.DEFAULT_ANON_UID, export.getAnonUid());
         assertEquals("default anongid not used", FsExport.DEFAULT_ANON_GID, export.getAnonUid());
     }
+
+    @Test
+    public void testWithDcap() throws UnknownHostException {
+	FsExport export = _exportFile.getExport("/with_dcap", InetAddress.getByName("192.169.2.2"));
+	assertTrue("dcap should be default option", export.isWithDcap());
+    }
+
+    @Test
+    public void testWithoutDcap() throws UnknownHostException {
+	FsExport export = _exportFile.getExport("/without_dcap", InetAddress.getByName("192.169.2.2"));
+	assertFalse("dcap is not disabled", export.isWithDcap());
+    }
+
 }

core/src/test/resources/org/dcache/nfs/exports
@@ -20,3 +20,5 @@
 /no_squash *
 /anon_access *(anonuid=500,anongid=501)
 /anon_access_default *
+/with_dcap *
+/without_dcap *(no_dcap)

commit 1c89149d3fdf2ad48b082088314891c5078bed44

vfs: fix regression introduced in 1c916c5
we have to consult export file on non pseudo inodes only.
This regression prevent clients to traverse PseudoFS.

Acked-by: Karsten Schwank
Target: master, 0.7

core/src/main/java/org/dcache/nfs/vfs/PseudoFs.java
@@ -155,18 +155,19 @@ public class PseudoFs implements VirtualFileSystem {
     public Inode lookup(Inode parent, String path) throws IOException {
         checkAccess(parent, ACE4_EXECUTE);
 
+        if (parent.isPesudoInode()) {
+            return lookupInPseudoDirectory(parent, path);
+        }
+
 	/*
-	 * REVISIT: this is not ;the best place to do it, but the simples one.
+	 * REVISIT: this is not the best place to do it, but the simples one.
 	 */
 	FsExport export = _exportFile.getExport(parent.exportIndex(), _inetAddress);
 	if (!export.isWithDcap() && ".(get)(cursor)".equals(path)) {
 	    throw new ChimeraNFSException(nfsstat.NFSERR_NOENT, "the dcap magic file is blocked");
 	}
 
-        if (parent.isPesudoInode()) {
-            return lookupInPseudoDirectory(parent, path);
-        }
-        return pushExportIndex(parent, _inner.lookup(parent, path));
+	return pushExportIndex(parent, _inner.lookup(parent, path));
     }
 
     @Override