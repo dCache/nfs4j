dCache/nfs4j

commit 45f0a6212bde0d9f05c1c6a3504adda2b381fdc6

nfs4: simplify nfstime4 structure
do not wrap seconds and nonos with int64 and int32 classes
Update fattr4_time_xxx to inherit from nfstime4.

Acked-by: Gerd Behrmann
Target: master

core/src/main/java/org/dcache/nfs/v4/xdr/nfstime4.java
@@ -22,8 +22,8 @@ import org.dcache.xdr.*;
 import java.io.IOException;
 
 public class nfstime4 implements XdrAble {
-    public int64_t seconds;
-    public uint32_t nseconds;
+    public long seconds;
+    public int nseconds;
 
     public nfstime4() {
     }
@@ -35,14 +35,14 @@ public class nfstime4 implements XdrAble {
 
     public void xdrEncode(XdrEncodingStream xdr)
            throws OncRpcException, IOException {
-        seconds.xdrEncode(xdr);
-        nseconds.xdrEncode(xdr);
+	xdr.xdrEncodeLong(seconds);
+	xdr.xdrEncodeLong(nseconds);
     }
 
     public void xdrDecode(XdrDecodingStream xdr)
            throws OncRpcException, IOException {
-        seconds = new int64_t(xdr);
-        nseconds = new uint32_t(xdr);
+        seconds = xdr.xdrDecodeLong();
+        nseconds = xdr.xdrDecodeInt();
     }
 
 }

core/src/main/java/org/dcache/nfs/v4/OperationEXCHANGE_ID.java
@@ -33,7 +33,6 @@ import org.dcache.nfs.v4.xdr.utf8str_cs;
 import org.dcache.nfs.v4.xdr.nfstime4;
 import org.dcache.nfs.v4.xdr.uint32_t;
 import org.dcache.nfs.v4.xdr.EXCHANGE_ID4resok;
-import org.dcache.nfs.v4.xdr.int64_t;
 import org.dcache.nfs.v4.xdr.verifier4;
 import org.dcache.nfs.v4.xdr.state_protect_how4;
 import org.dcache.nfs.v4.xdr.EXCHANGE_ID4res;
@@ -46,6 +45,7 @@ import java.net.URL;
 import java.security.CodeSource;
 import java.security.Principal;
 import java.security.ProtectionDomain;
+import java.util.concurrent.TimeUnit;
 import java.util.jar.Attributes;
 import java.util.jar.JarInputStream;
 import java.util.jar.Manifest;
@@ -248,8 +248,8 @@ public class OperationEXCHANGE_ID extends AbstractNFSv4Operation {
         res.eir_resok4.eir_server_impl_id[0].nii_domain = new utf8str_cis(NFS4_IMPLEMENTATION_DOMAIN);
         res.eir_resok4.eir_server_impl_id[0].nii_name = new utf8str_cs(NFS4_IMPLEMENTATION_ID + " build-time " + COMPILTE_TIME);
         nfstime4 releaseDate = new nfstime4();
-        releaseDate.nseconds = new uint32_t(0);
-        releaseDate.seconds = new int64_t(System.currentTimeMillis() / 1000);
+        releaseDate.nseconds = 0;
+        releaseDate.seconds = TimeUnit.MILLISECONDS.toSeconds(System.currentTimeMillis());
         res.eir_resok4.eir_server_impl_id[0].nii_date = releaseDate;
 
         res.eir_resok4.eir_state_protect = new state_protect4_r();

core/src/main/java/org/dcache/nfs/v4/OperationGETATTR.java
@@ -39,7 +39,6 @@ import org.dcache.nfs.v4.xdr.fattr4_unique_handles;
 import org.dcache.nfs.v4.xdr.fattr4_lease_time;
 import org.dcache.nfs.v4.xdr.uint64_t;
 import org.dcache.nfs.v4.xdr.fattr4_fh_expire_type;
-import org.dcache.nfs.v4.xdr.int64_t;
 import org.dcache.nfs.v4.xdr.nfs_opnum4;
 import org.dcache.nfs.v4.xdr.nfsace4;
 import org.dcache.nfs.v4.xdr.fattr4_named_attr;
@@ -66,7 +65,6 @@ import org.dcache.nfs.v4.xdr.fattr4_size;
 import org.dcache.nfs.v4.xdr.fattr4_files_total;
 import org.dcache.nfs.v4.xdr.fattr4_filehandle;
 import org.dcache.nfs.v4.xdr.fattr4;
-import org.dcache.nfs.v4.xdr.nfstime4;
 import org.dcache.nfs.v4.xdr.fattr4_link_support;
 import org.dcache.nfs.v4.xdr.fattr4_time_modify;
 import org.dcache.nfs.v4.xdr.fattr4_no_trunc;
@@ -328,29 +326,29 @@ public class OperationGETATTR extends AbstractNFSv4Operation {
             case nfs4_prot.FATTR4_SYSTEM:
                 return Optional.of(new fattr4_system(false));
             case nfs4_prot.FATTR4_TIME_ACCESS:
-                nfstime4 atime = new nfstime4();
-                atime.seconds = new int64_t(TimeUnit.SECONDS.convert(stat.getATime(), TimeUnit.MILLISECONDS));
-                atime.nseconds = new uint32_t(0);
-                return Optional.of(new fattr4_time_access(atime));
+                fattr4_time_access atime = new fattr4_time_access();
+                atime.seconds = TimeUnit.MILLISECONDS.toSeconds(stat.getATime());
+                atime.nseconds = 0;
+                return Optional.of(atime);
             case nfs4_prot.FATTR4_TIME_BACKUP:
                 return Optional.absent();
             case nfs4_prot.FATTR4_TIME_CREATE:
-                nfstime4 ctime = new nfstime4();
-                ctime.seconds = new int64_t(TimeUnit.SECONDS.convert(stat.getCTime(), TimeUnit.MILLISECONDS));
-                ctime.nseconds = new uint32_t(0);
-                return Optional.of(new fattr4_time_create(ctime));
+                fattr4_time_create ctime = new fattr4_time_create();
+                ctime.seconds = TimeUnit.MILLISECONDS.toSeconds(stat.getCTime());
+                ctime.nseconds = 0;
+                return Optional.of(ctime);
             case nfs4_prot.FATTR4_TIME_DELTA:
                 return Optional.absent();
             case nfs4_prot.FATTR4_TIME_METADATA:
-                nfstime4 mdtime = new nfstime4();
-                mdtime.seconds = new int64_t(TimeUnit.SECONDS.convert(stat.getCTime(), TimeUnit.MILLISECONDS));
-                mdtime.nseconds = new uint32_t(0);
-                return Optional.of(new fattr4_time_metadata(mdtime));
+                fattr4_time_metadata mdtime = new fattr4_time_metadata();
+                mdtime.seconds = TimeUnit.MILLISECONDS.toSeconds(stat.getCTime());
+                mdtime.nseconds = 0;
+                return Optional.of(mdtime);
             case nfs4_prot.FATTR4_TIME_MODIFY:
-                nfstime4 mtime = new nfstime4();
-                mtime.seconds = new int64_t(TimeUnit.SECONDS.convert(stat.getMTime(), TimeUnit.MILLISECONDS));
-                mtime.nseconds = new uint32_t(0);
-                return Optional.of(new fattr4_time_modify(mtime));
+                fattr4_time_modify mtime = new fattr4_time_modify();
+                mtime.seconds = TimeUnit.MILLISECONDS.toSeconds(stat.getMTime());
+                mtime.nseconds = 0;
+                return Optional.of(mtime);
             case nfs4_prot.FATTR4_MOUNTED_ON_FILEID:
 
                 /*

core/src/main/java/org/dcache/nfs/v4/OperationSETATTR.java
@@ -194,8 +194,8 @@ public class OperationSETATTR extends AbstractNFSv4Operation {
             case nfs4_prot.FATTR4_TIME_CREATE :
                 nfstime4 ctime = new nfstime4();
                 ctime.xdrDecode(xdr);
-                stat.setCTime( TimeUnit.MILLISECONDS.convert(ctime.seconds.value, TimeUnit.SECONDS) +
-                        TimeUnit.MILLISECONDS.convert(ctime.nseconds.value, TimeUnit.NANOSECONDS));
+                stat.setCTime( TimeUnit.MILLISECONDS.convert(ctime.seconds, TimeUnit.SECONDS) +
+                        TimeUnit.MILLISECONDS.convert(ctime.nseconds, TimeUnit.NANOSECONDS));
                 isApplied = true;
                 break;
             case nfs4_prot.FATTR4_TIME_MODIFY_SET :
@@ -206,8 +206,8 @@ public class OperationSETATTR extends AbstractNFSv4Operation {
                 if( setMtime.set_it == time_how4.SET_TO_SERVER_TIME4 ) {
                     realMtime = System.currentTimeMillis();
                 }else{
-                    realMtime = TimeUnit.MILLISECONDS.convert(setMtime.time.seconds.value, TimeUnit.SECONDS) +
-                            TimeUnit.MILLISECONDS.convert(setMtime.time.nseconds.value, TimeUnit.NANOSECONDS);
+                    realMtime = TimeUnit.MILLISECONDS.convert(setMtime.time.seconds, TimeUnit.SECONDS) +
+                            TimeUnit.MILLISECONDS.convert(setMtime.time.nseconds, TimeUnit.NANOSECONDS);
                 }
 
                 stat.setMTime( realMtime );

core/src/main/java/org/dcache/nfs/v4/client/CompoundBuilder.java
@@ -82,7 +82,6 @@ import org.dcache.nfs.v4.xdr.nfstime4;
 import org.dcache.nfs.v4.xdr.layoutreturn_type4;
 import org.dcache.nfs.v4.xdr.nfs_cookie4;
 import org.dcache.nfs.v4.xdr.layouttype4;
-import org.dcache.nfs.v4.xdr.int64_t;
 import org.dcache.nfs.v4.xdr.verifier4;
 import org.dcache.nfs.v4.xdr.sessionid4;
 import org.dcache.nfs.v4.xdr.openflag4;
@@ -93,6 +92,7 @@ import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 import org.dcache.utils.Bytes;
 import org.dcache.xdr.OncRpcException;
 import org.dcache.xdr.XdrBuffer;
@@ -177,8 +177,8 @@ public class CompoundBuilder {
         op.opexchange_id.eia_client_impl_id[0] = n4;
 
         nfstime4 releaseDate = new nfstime4();
-        releaseDate.nseconds = new uint32_t(0);
-        releaseDate.seconds = new int64_t(System.currentTimeMillis() / 1000);
+        releaseDate.nseconds = 0;
+        releaseDate.seconds = TimeUnit.MILLISECONDS.toSeconds(System.currentTimeMillis());
 
         op.opexchange_id.eia_client_impl_id[0].nii_date = releaseDate;
         op.opexchange_id.eia_clientowner = new client_owner4();
@@ -186,7 +186,7 @@ public class CompoundBuilder {
 
         op.opexchange_id.eia_clientowner.co_verifier = new verifier4();
         op.opexchange_id.eia_clientowner.co_verifier.value = new byte[nfs4_prot.NFS4_VERIFIER_SIZE];
-        Bytes.putLong(op.opexchange_id.eia_clientowner.co_verifier.value, 0, releaseDate.seconds.value);
+        Bytes.putLong(op.opexchange_id.eia_clientowner.co_verifier.value, 0, releaseDate.seconds);
 
         op.opexchange_id.eia_flags = new uint32_t(flags);
         op.opexchange_id.eia_state_protect = new state_protect4_a();

core/src/main/java/org/dcache/nfs/v4/client/ExchangeIDStub.java
@@ -30,10 +30,10 @@ import org.dcache.nfs.v4.xdr.utf8str_cs;
 import org.dcache.nfs.v4.xdr.EXCHANGE_ID4args;
 import org.dcache.nfs.v4.xdr.nfstime4;
 import org.dcache.nfs.v4.xdr.uint32_t;
-import org.dcache.nfs.v4.xdr.int64_t;
 import org.dcache.nfs.v4.xdr.verifier4;
 import org.dcache.nfs.v4.xdr.client_owner4;
 import java.util.Random;
+import java.util.concurrent.TimeUnit;
 import org.dcache.utils.Bytes;
 
 public class ExchangeIDStub {
@@ -53,8 +53,8 @@ public class ExchangeIDStub {
         op.opexchange_id.eia_client_impl_id[0] = n4;
 
         nfstime4 releaseDate = new nfstime4();
-        releaseDate.nseconds = new uint32_t(0);
-        releaseDate.seconds = new int64_t(System.currentTimeMillis() / 1000);
+        releaseDate.nseconds = 0;
+        releaseDate.seconds = TimeUnit.MILLISECONDS.toSeconds(System.currentTimeMillis());
 
         op.opexchange_id.eia_client_impl_id[0].nii_date = releaseDate;
         op.opexchange_id.eia_clientowner = new client_owner4();

core/src/main/java/org/dcache/nfs/v4/xdr/fattr4_time_access.java
@@ -21,30 +21,15 @@ package org.dcache.nfs.v4.xdr;
 import org.dcache.xdr.*;
 import java.io.IOException;
 
-public class fattr4_time_access implements XdrAble {
+public class fattr4_time_access extends nfstime4 {
 
-    public nfstime4 value;
 
     public fattr4_time_access() {
-    }
-
-    public fattr4_time_access(nfstime4 value) {
-        this.value = value;
     }
 
     public fattr4_time_access(XdrDecodingStream xdr)
            throws OncRpcException, IOException {
         xdrDecode(xdr);
-    }
-
-    public void xdrEncode(XdrEncodingStream xdr)
-           throws OncRpcException, IOException {
-        value.xdrEncode(xdr);
-    }
-
-    public void xdrDecode(XdrDecodingStream xdr)
-           throws OncRpcException, IOException {
-        value = new nfstime4(xdr);
     }
 
 }

core/src/main/java/org/dcache/nfs/v4/xdr/fattr4_time_create.java
@@ -21,30 +21,14 @@ package org.dcache.nfs.v4.xdr;
 import org.dcache.xdr.*;
 import java.io.IOException;
 
-public class fattr4_time_create implements XdrAble {
-
-    public nfstime4 value;
+public class fattr4_time_create extends nfstime4 {
 
     public fattr4_time_create() {
-    }
-
-    public fattr4_time_create(nfstime4 value) {
-        this.value = value;
     }
 
     public fattr4_time_create(XdrDecodingStream xdr)
            throws OncRpcException, IOException {
         xdrDecode(xdr);
-    }
-
-    public void xdrEncode(XdrEncodingStream xdr)
-           throws OncRpcException, IOException {
-        value.xdrEncode(xdr);
-    }
-
-    public void xdrDecode(XdrDecodingStream xdr)
-           throws OncRpcException, IOException {
-        value = new nfstime4(xdr);
     }
 
 }

commit 00118a88df97e1016cf48d653461aa6f250d3549

nfs4: fix nfstime4 encoding
fix copy-paste regression introduced in 45f0a62

Acked-by: Paul Millar
Target: master

core/src/main/java/org/dcache/nfs/v4/xdr/nfstime4.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2009 - 2012 Deutsches Elektronen-Synchroton,
+ * Copyright (c) 2009 - 2014 Deutsches Elektronen-Synchroton,
  * Member of the Helmholtz Association, (DESY), HAMBURG, GERMANY
  *
  * This library is free software; you can redistribute it and/or modify
@@ -45,7 +45,7 @@ public class nfstime4 implements XdrAble {
     public void xdrEncode(XdrEncodingStream xdr)
            throws OncRpcException, IOException {
 	xdr.xdrEncodeLong(seconds);
-	xdr.xdrEncodeLong(nseconds);
+	xdr.xdrEncodeInt(nseconds);
     }
 
     public void xdrDecode(XdrDecodingStream xdr)