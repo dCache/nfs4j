dCache/nfs4j

commit 7e865b591e16c69bfd9ad4cc48e2d89998eaaea3

nfs4: simplify data types
the next portion of changes

Acked-by: Albert Rossi
Target: master

core/src/main/java/org/dcache/nfs/v4/xdr/fattr4_lease_time.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2009 - 2012 Deutsches Elektronen-Synchroton,
+ * Copyright (c) 2009 - 2014 Deutsches Elektronen-Synchroton,
  * Member of the Helmholtz Association, (DESY), HAMBURG, GERMANY
  *
  * This library is free software; you can redistribute it and/or modify
@@ -21,31 +21,18 @@ package org.dcache.nfs.v4.xdr;
 import org.dcache.xdr.*;
 import java.io.IOException;
 
-public class fattr4_lease_time implements XdrAble {
-
-    public nfs_lease4 value;
+public class fattr4_lease_time extends nfs_lease4 {
 
     public fattr4_lease_time() {
     }
 
-    public fattr4_lease_time(nfs_lease4 value) {
-        this.value = value;
+    public fattr4_lease_time(int value) {
+        super();
     }
 
     public fattr4_lease_time(XdrDecodingStream xdr)
            throws OncRpcException, IOException {
-        xdrDecode(xdr);
+        super(xdr);
     }
-
-    public void xdrEncode(XdrEncodingStream xdr)
-           throws OncRpcException, IOException {
-        value.xdrEncode(xdr);
-    }
-
-    public void xdrDecode(XdrDecodingStream xdr)
-           throws OncRpcException, IOException {
-        value = new nfs_lease4(xdr);
-    }
-
 }
 // End of fattr4_lease_time.java

core/src/main/java/org/dcache/nfs/v4/xdr/nfs_lease4.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2009 - 2012 Deutsches Elektronen-Synchroton,
+ * Copyright (c) 2009 - 2014 Deutsches Elektronen-Synchroton,
  * Member of the Helmholtz Association, (DESY), HAMBURG, GERMANY
  *
  * This library is free software; you can redistribute it and/or modify
@@ -21,31 +21,18 @@ package org.dcache.nfs.v4.xdr;
 import org.dcache.xdr.*;
 import java.io.IOException;
 
-public class nfs_lease4 implements XdrAble {
-
-    public uint32_t value;
+public class nfs_lease4 extends uint32_t {
 
     public nfs_lease4() {
     }
 
-    public nfs_lease4(uint32_t value) {
+    public nfs_lease4(int value) {
         this.value = value;
     }
 
     public nfs_lease4(XdrDecodingStream xdr)
            throws OncRpcException, IOException {
-        xdrDecode(xdr);
+        super(xdr);
     }
-
-    public void xdrEncode(XdrEncodingStream xdr)
-           throws OncRpcException, IOException {
-        value.xdrEncode(xdr);
-    }
-
-    public void xdrDecode(XdrDecodingStream xdr)
-           throws OncRpcException, IOException {
-        value = new uint32_t(xdr);
-    }
-
 }
 // End of nfs_lease4.java

core/src/main/java/org/dcache/nfs/v4/ClientCB.java
@@ -115,7 +115,7 @@ class ClientCB {
         CB_SEQUENCE4args cbSequence = new CB_SEQUENCE4args();
         cbSequence.csa_cachethis = false;
         cbSequence.csa_highest_slotid = new slotid4(_highestSlotId);
-        cbSequence.csa_sequenceid = new sequenceid4(new uint32_t(_sequenceid));
+        cbSequence.csa_sequenceid = new sequenceid4(_sequenceid);
         cbSequence.csa_slotid = new slotid4(0);
         cbSequence.csa_sessionid = _session;
         cbSequence.csa_referring_call_lists = new referring_call_list4[0];

core/src/main/java/org/dcache/nfs/v4/OperationCREATE_SESSION.java
@@ -48,7 +48,7 @@ public class OperationCREATE_SESSION extends AbstractNFSv4Operation {
     public void process(CompoundContext context, nfs_resop4 result) throws ChimeraNFSException {
         final CREATE_SESSION4res res = result.opcreate_session;
 
-        Long clientId = Long.valueOf(_args.opcreate_session.csa_clientid.value.value);
+        Long clientId = _args.opcreate_session.csa_clientid.value;
 
         /*
          * check for correct arguments
@@ -104,7 +104,7 @@ public class OperationCREATE_SESSION extends AbstractNFSv4Operation {
             throw new ChimeraNFSException(nfsstat.NFSERR_CLID_INUSE, "client already in use: " + client.principal() + " " + context.getPrincipal());
         }
 
-        NFSv41Session session = client.createSession(_args.opcreate_session.csa_sequence.value.value,
+        NFSv41Session session = client.createSession(_args.opcreate_session.csa_sequence.value,
                 _args.opcreate_session.csa_fore_chan_attrs.ca_maxrequests.value);
         _log.debug("adding new session [{}]", session);
         context.getStateHandler().sessionById(session.id(), session);

core/src/main/java/org/dcache/nfs/v4/OperationDESTROY_CLIENTID.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2009 - 2012 Deutsches Elektronen-Synchroton,
+ * Copyright (c) 2009 - 2014 Deutsches Elektronen-Synchroton,
  * Member of the Helmholtz Association, (DESY), HAMBURG, GERMANY
  *
  * This library is free software; you can redistribute it and/or modify
@@ -37,7 +37,7 @@ public class OperationDESTROY_CLIENTID extends AbstractNFSv4Operation {
 
         final DESTROY_CLIENTID4res res = result.opdestroy_clientid;
 
-        Long clientId = Long.valueOf(_args.opdestroy_clientid.dca_clientid.value.value);
+        Long clientId = _args.opdestroy_clientid.dca_clientid.value;
 
         NFSv4StateHandler stateHandler = context.getStateHandler();
         NFS4Client client = stateHandler.getClientByID(clientId);

core/src/main/java/org/dcache/nfs/v4/OperationEXCHANGE_ID.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2009 - 2012 Deutsches Elektronen-Synchroton,
+ * Copyright (c) 2009 - 2014 Deutsches Elektronen-Synchroton,
  * Member of the Helmholtz Association, (DESY), HAMBURG, GERMANY
  *
  * This library is free software; you can redistribute it and/or modify
@@ -21,7 +21,6 @@ package org.dcache.nfs.v4;
 
 import org.dcache.nfs.v4.xdr.nfs_opnum4;
 import org.dcache.nfs.v4.xdr.nfs_argop4;
-import org.dcache.nfs.v4.xdr.uint64_t;
 import org.dcache.nfs.v4.xdr.nfs_impl_id4;
 import org.dcache.nfs.v4.xdr.utf8str_cis;
 import org.dcache.nfs.v4.xdr.state_protect4_r;
@@ -234,8 +233,8 @@ public class OperationEXCHANGE_ID extends AbstractNFSv4Operation {
         client.updateLeaseTime();
 
         res.eir_resok4 = new EXCHANGE_ID4resok();
-        res.eir_resok4.eir_clientid = new clientid4(new uint64_t(client.getId()));
-        res.eir_resok4.eir_sequenceid = new sequenceid4(new uint32_t(client.currentSeqID()));
+        res.eir_resok4.eir_clientid = new clientid4(client.getId());
+        res.eir_resok4.eir_sequenceid = new sequenceid4(client.currentSeqID());
         res.eir_resok4.eir_flags = new uint32_t(_flag);
 
         ServerIdProvider serverIdProvider = context.getServerIdProvider();

core/src/main/java/org/dcache/nfs/v4/OperationGETATTR.java
@@ -28,7 +28,6 @@ import org.dcache.nfs.v4.xdr.fattr4_aclsupport;
 import org.dcache.nfs.v4.xdr.nfs_ftype4;
 import org.dcache.nfs.v4.xdr.attrlist4;
 import org.dcache.nfs.v4.xdr.fattr4_case_insensitive;
-import org.dcache.nfs.v4.xdr.nfs_lease4;
 import org.dcache.nfs.v4.xdr.nfs_fh4;
 import org.dcache.nfs.v4.xdr.fattr4_rawdev;
 import org.dcache.nfs.v4.xdr.fattr4_maxname;
@@ -198,8 +197,7 @@ public class OperationGETATTR extends AbstractNFSv4Operation {
                 fattr4_type type = new fattr4_type(unixType2NFS(stat.getMode()));
                 return Optional.of(type);
             case nfs4_prot.FATTR4_FH_EXPIRE_TYPE:
-                uint32_t fh_type = new uint32_t(nfs4_prot.FH4_PERSISTENT);
-                fattr4_fh_expire_type fh_expire_type = new fattr4_fh_expire_type(fh_type);
+                fattr4_fh_expire_type fh_expire_type = new fattr4_fh_expire_type(nfs4_prot.FH4_PERSISTENT);
                 return Optional.of(fh_expire_type);
             case nfs4_prot.FATTR4_CHANGE:
                 fattr4_change change = new fattr4_change(stat.getCTime());
@@ -224,7 +222,7 @@ public class OperationGETATTR extends AbstractNFSv4Operation {
             case nfs4_prot.FATTR4_UNIQUE_HANDLES:
                 return Optional.of(new fattr4_unique_handles(true));
             case nfs4_prot.FATTR4_LEASE_TIME:
-                return Optional.of(new fattr4_lease_time(new nfs_lease4(new uint32_t(NFSv4Defaults.NFS4_LEASE_TIME))));
+                return Optional.of(new fattr4_lease_time(NFSv4Defaults.NFS4_LEASE_TIME));
             case nfs4_prot.FATTR4_RDATTR_ERROR:
                 //enum is an integer
                 return Optional.of(new fattr4_rdattr_error(0));
@@ -236,10 +234,8 @@ public class OperationGETATTR extends AbstractNFSv4Operation {
                 nfsace4[] aces = context.getFs().getAcl(inode);
                 return Optional.of(new fattr4_acl(aces));
             case nfs4_prot.FATTR4_ACLSUPPORT:
-                fattr4_aclsupport aclSupport = new fattr4_aclsupport();
-                aclSupport.value = new uint32_t();
-                aclSupport.value.value = nfs4_prot.ACL4_SUPPORT_ALLOW_ACL
-                        | nfs4_prot.ACL4_SUPPORT_DENY_ACL;
+                fattr4_aclsupport aclSupport = new fattr4_aclsupport(
+			nfs4_prot.ACL4_SUPPORT_ALLOW_ACL| nfs4_prot.ACL4_SUPPORT_DENY_ACL);
                 return Optional.of(aclSupport);
             case nfs4_prot.FATTR4_ARCHIVE:
                 return Optional.absent();
@@ -279,7 +275,7 @@ public class OperationGETATTR extends AbstractNFSv4Operation {
             case nfs4_prot.FATTR4_MAXREAD:
                 return Optional.of(new fattr4_maxread(NFSv4Defaults.NFS4_MAXIOBUFFERSIZE));
             case nfs4_prot.FATTR4_MAXWRITE:
-                fattr4_maxwrite maxwrite = new fattr4_maxwrite(new uint64_t(NFSv4Defaults.NFS4_MAXIOBUFFERSIZE));
+                fattr4_maxwrite maxwrite = new fattr4_maxwrite(NFSv4Defaults.NFS4_MAXIOBUFFERSIZE);
                 return Optional.of(maxwrite);
             case nfs4_prot.FATTR4_MIMETYPE:
                 return Optional.absent();
@@ -306,8 +302,8 @@ public class OperationGETATTR extends AbstractNFSv4Operation {
                 return Optional.absent();
             case nfs4_prot.FATTR4_RAWDEV:
                 specdata4 dev = new specdata4();
-                dev.specdata1 = new uint32_t(0);
-                dev.specdata2 = new uint32_t(0);
+                dev.specdata1 = 0;
+                dev.specdata2 = 0;
                 return Optional.of(new fattr4_rawdev(dev));
             case nfs4_prot.FATTR4_SPACE_AVAIL:
                 fsStat = getFsStat(fsStat, fs);
@@ -352,8 +348,7 @@ public class OperationGETATTR extends AbstractNFSv4Operation {
                     mofi = 0x12345678;
                 }
 
-                uint64_t rootid = new uint64_t(mofi);
-                fattr4_mounted_on_fileid mounted_on_fileid = new fattr4_mounted_on_fileid(rootid);
+                fattr4_mounted_on_fileid mounted_on_fileid = new fattr4_mounted_on_fileid(mofi);
                 return Optional.of(mounted_on_fileid);
 
             /**

core/src/main/java/org/dcache/nfs/v4/OperationOPEN.java
@@ -58,7 +58,7 @@ public class OperationOPEN extends AbstractNFSv4Operation {
         final OPEN4res res = result.opopen;
 
         try {
-            Long clientid = Long.valueOf(_args.opopen.owner.value.clientid.value.value);
+            Long clientid = _args.opopen.owner.value.clientid.value;
             NFS4Client client;
 
             if (context.getMinorversion() > 0) {

commit cde864881b0de40ac811c310e461717e50b23b86

nfsv4: fix regression introduced in 7e865b5
Acked-by: Gerd Behrmann
Target: master

core/src/main/java/org/dcache/nfs/v4/xdr/fattr4_lease_time.java
@@ -27,7 +27,7 @@ public class fattr4_lease_time extends nfs_lease4 {
     }
 
     public fattr4_lease_time(int value) {
-        super();
+        super(value);
     }
 
     public fattr4_lease_time(XdrDecodingStream xdr)

core/src/main/java/org/dcache/nfs/v4/xdr/nfs_lease4.java
@@ -27,7 +27,7 @@ public class nfs_lease4 extends uint32_t {
     }
 
     public nfs_lease4(int value) {
-        this.value = value;
+        super(value);
     }
 
     public nfs_lease4(XdrDecodingStream xdr)