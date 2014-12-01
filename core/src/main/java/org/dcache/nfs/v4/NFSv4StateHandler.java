1e77d96b38440ae34220ca720668f3caffdf311c
Tigran Mkrtchyan <tigran.mkrtchyan@desy.de>
2014-12-01 12:16:38 +0100

    nfsv4: disable grace period
    Grace period is a time window given to clients to recover
    open files and lock on server restart.
    
    As we do not have a persistent storage for state information,
    grace period makes no sense as it ends up as a simple delay
    before first IO request can be processed.
    
    Acked-by: Gerd Behrmann
    Target: master, 0.9, 0.8
    (cherry picked from commit 89a66f8f1bfa9dea483f77ff5c8781ddcded870b)
    Signed-off-by: Tigran Mkrtchyan <tigran.mkrtchyan@desy.de>

--- core/src/main/java/org/dcache/nfs/v4/NFSv4StateHandler.java
+++ core/src/main/java/org/dcache/nfs/v4/NFSv4StateHandler.java
@@ -193,6 +193,11 @@ public class NFSv4StateHandler {
      * @return true, if grace period expired.
      */
     public boolean hasGracePeriodExpired() {
-	return _bootTime + TimeUnit.SECONDS.toMillis(NFSv4Defaults.NFS4_LEASE_TIME) < System.currentTimeMillis();
+	/*
+	 * As we do not have a persistent storage for state information,
+	 * grace period makes no sense as it ends up as a simple delay
+	 * before first IO request can be processed.
+	 */
+	return true;
     }
 }
