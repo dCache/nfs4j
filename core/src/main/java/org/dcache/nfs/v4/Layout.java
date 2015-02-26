314994e486dd3cd8bcf49797cdf5f7fc51ada394
Tigran Mkrtchyan <tigran.mkrtchyan@desy.de>
2015-02-27 00:11:59 +0100

    nfs4: introduce Layouts utility class to create and manipulate layouts
    As we expect more layout types to be implemented, make an utility class
    to create different layouts.
    
    Target: master
    Acked-By: Karsten Schwank

--- core/src/main/java/org/dcache/nfs/v4/Layout.java
+++ core/src/main/java/org/dcache/nfs/v4/Layout.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2009 - 2012 Deutsches Elektronen-Synchroton,
+ * Copyright (c) 2009 - 2015 Deutsches Elektronen-Synchroton,
  * Member of the Helmholtz Association, (DESY), HAMBURG, GERMANY
  *
  * This library is free software; you can redistribute it and/or modify
@@ -27,13 +27,10 @@ import org.dcache.nfs.v4.xdr.layout4;
 import org.dcache.nfs.v4.xdr.layout_content4;
 import org.dcache.nfs.v4.xdr.layouttype4;
 import org.dcache.nfs.v4.xdr.length4;
-import org.dcache.nfs.v4.xdr.nfl_util4;
-import org.dcache.nfs.v4.xdr.nfs4_prot;
 import org.dcache.nfs.v4.xdr.nfs_fh4;
 import org.dcache.nfs.status.ServerFaultException;
 import org.dcache.nfs.v4.xdr.nfsv4_1_file_layout4;
 import org.dcache.nfs.v4.xdr.offset4;
-import org.dcache.nfs.v4.xdr.uint32_t;
 import org.dcache.xdr.XdrBuffer;
 import org.glassfish.grizzly.Buffer;
 
@@ -100,33 +97,8 @@ public class Layout {
     }
 
     private static layout_content4 getSegmentContent(deviceid4 deviceid, int stripeSize, nfs_fh4 fh) throws ChimeraNFSException {
-        nfsv4_1_file_layout4 layout = new nfsv4_1_file_layout4();
 
-        layout.nfl_deviceid = deviceid;
-
-        /*
-         * The number of elements in nfl_fh_list MUST be one of three values:
-         *
-         *  Zero.  This means that filehandles used for each data
-         *  server are the same as the filehandle returned by the OPEN
-         *  operation from the metadata server.
-         *
-         *  One.  This means that every data server uses the same
-         *  filehandle: what is specified in nfl_fh_list[0].
-         *
-         *  The same number of elements in nflda_multipath_ds_list.
-         *  Thus, in this case, when sending an I/O operation to any
-         *  data server in nflda_multipath_ds_list[X], the filehandle
-         *  in nfl_fh_list[X] MUST be used.
-         */
-        layout.nfl_fh_list = new nfs_fh4[1];
-        layout.nfl_fh_list[0] = fh;
-
-        layout.nfl_first_stripe_index = new uint32_t(0);
-        layout.nfl_util = new nfl_util4(stripeSize & nfs4_prot.NFL4_UFLG_STRIPE_UNIT_SIZE_MASK);
-
-        //where the striping pattern starts
-        layout.nfl_pattern_offset = new offset4(0);
+        nfsv4_1_file_layout4 layout = Layouts.newNfsFileLayout(deviceid, stripeSize, fh);
 
         XdrBuffer xdr = new XdrBuffer(512);
         xdr.beginEncoding();
