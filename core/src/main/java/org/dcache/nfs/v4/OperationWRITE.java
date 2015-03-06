dCache/nfs4j

commit 6dc099bc46a22b9a0af6b29753493fb287b0ee55

allow VFS access to write stability parameters. chimera VFS always returns FILE_SYNC, as before (issue #12)

core/src/main/java/org/dcache/nfs/v4/OperationWRITE.java
@@ -36,6 +36,7 @@ import org.dcache.nfs.status.IsDirException;
 import org.dcache.nfs.status.NfsIoException;
 import org.dcache.nfs.v4.xdr.nfs_resop4;
 import org.dcache.nfs.vfs.Stat;
+import org.dcache.nfs.vfs.VirtualFileSystem;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -77,16 +78,16 @@ public class OperationWRITE extends AbstractNFSv4Operation {
         byte[] data = new byte[count];
         _args.opwrite.data.get(data);
 
-        int bytesWritten = context.getFs().write(context.currentInode(),
-                data, offset, count);
+        VirtualFileSystem.WriteResult writeResult = context.getFs().write(context.currentInode(),
+                data, offset, count, VirtualFileSystem.StabilityLevel.fromStableHow(_args.opwrite.stable));
 
-        if (bytesWritten < 0) {
+        if (writeResult.getBytesWritten() < 0) {
             throw new NfsIoException("IO not allowed");
         }
 
         res.status = nfsstat.NFS_OK;
         res.resok4 = new WRITE4resok();
-        res.resok4.count = new count4(bytesWritten);
+        res.resok4.count = new count4(writeResult.getStabilityLevel().toStableHow());
         res.resok4.committed = stable_how4.FILE_SYNC4;
         res.resok4.writeverf = new verifier4();
         res.resok4.writeverf.value = new byte[nfs4_prot.NFS4_VERIFIER_SIZE];

core/src/main/java/org/dcache/nfs/v3/NfsServerV3.java
@@ -35,7 +35,6 @@ import org.dcache.nfs.v3.xdr.post_op_fh3;
 import org.dcache.nfs.v3.xdr.READLINK3args;
 import org.dcache.nfs.v3.xdr.uint64;
 import org.dcache.nfs.v3.xdr.MKDIR3res;
-import org.dcache.nfs.v3.xdr.stable_how;
 import org.dcache.nfs.v3.xdr.WRITE3args;
 import org.dcache.nfs.v3.xdr.createmode3;
 import org.dcache.nfs.v3.xdr.post_op_attr;
@@ -1370,12 +1369,13 @@ public class NfsServerV3 extends nfs3_protServerStub {
             res.resok = new WRITE3resok();
             res.status = nfsstat.NFS_OK;
 
-            int ret = fs.write(inode, arg1.data, offset, count);
-            if (ret < 0) {
+            VirtualFileSystem.StabilityLevel requiredStabilityLevel = VirtualFileSystem.StabilityLevel.fromStableHow(arg1.stable);
+            VirtualFileSystem.WriteResult ret = fs.write(inode, arg1.data, offset, count, requiredStabilityLevel);
+            if (ret.getBytesWritten() < 0) {
                 throw new NfsIoException("IO not allowed");
             }
 
-            res.resok.count = new count3(new uint32(ret));
+            res.resok.count = new count3(new uint32(ret.getBytesWritten()));
             res.resok.file_wcc = new wcc_data();
             res.resok.file_wcc.after = new post_op_attr();
             res.resok.file_wcc.after.attributes_follow = true;
@@ -1384,7 +1384,7 @@ public class NfsServerV3 extends nfs3_protServerStub {
             HimeraNfsUtils.fill_attributes(fs.getattr(inode), res.resok.file_wcc.after.attributes);
             res.resok.file_wcc.before = new pre_op_attr();
             res.resok.file_wcc.before.attributes_follow = false;
-            res.resok.committed = stable_how.FILE_SYNC;
+            res.resok.committed = ret.getStabilityLevel().toStableHow();
             res.resok.verf = new writeverf3();
             res.resok.verf.value = new byte[nfs3_prot.NFS3_WRITEVERFSIZE];
         } catch (ChimeraNFSException hne) {

core/src/main/java/org/dcache/nfs/vfs/ChimeraVfs.java
@@ -182,9 +182,10 @@ public class ChimeraVfs implements VirtualFileSystem, AclCheckable {
     }
 
     @Override
-    public int write(Inode inode, byte[] data, long offset, int count) throws IOException {
+    public WriteResult write(Inode inode, byte[] data, long offset, int count, StabilityLevel stabilityLevel) throws IOException {
         FsInode fsInode = toFsInode(inode);
-        return fsInode.write(offset, data, 0, count);
+        int bytesWritten = fsInode.write(offset, data, 0, count);
+        return new WriteResult(StabilityLevel.FILE_SYNC, bytesWritten);
     }
 
     @Override

core/src/main/java/org/dcache/nfs/vfs/PseudoFs.java
@@ -275,9 +275,9 @@ public class PseudoFs implements VirtualFileSystem {
     }
 
     @Override
-    public int write(Inode inode, byte[] data, long offset, int count) throws IOException {
+    public WriteResult write(Inode inode, byte[] data, long offset, int count, StabilityLevel stabilityLevel) throws IOException {
         checkAccess(inode, ACE4_WRITE_DATA);
-        return _inner.write(inode, data, offset, count);
+        return _inner.write(inode, data, offset, count, stabilityLevel);
     }
 
     @Override

core/src/main/java/org/dcache/nfs/vfs/VfsCache.java
@@ -73,8 +73,8 @@ public class VfsCache implements VirtualFileSystem {
     }
 
     @Override
-    public int write(Inode inode, byte[] data, long offset, int count) throws IOException {
-        return _inner.write(inode, data, offset, count);
+    public WriteResult write(Inode inode, byte[] data, long offset, int count, StabilityLevel stabilityLevel) throws IOException {
+        return _inner.write(inode, data, offset, count, stabilityLevel);
     }
 
     @Override

core/src/main/java/org/dcache/nfs/vfs/VirtualFileSystem.java
@@ -22,6 +22,7 @@ package org.dcache.nfs.vfs;
 import java.io.IOException;
 import java.util.List;
 import org.dcache.nfs.v4.xdr.nfsace4;
+import org.dcache.nfs.v4.xdr.stable_how4;
 
 public interface VirtualFileSystem {
 
@@ -53,7 +54,7 @@ public interface VirtualFileSystem {
 
     Inode symlink(Inode parent, String path, String link, int uid, int gid, int mode) throws IOException;
 
-    int write(Inode inode, byte[] data, long offset, int count) throws IOException;
+    WriteResult write(Inode inode, byte[] data, long offset, int count, StabilityLevel stabilityLevel) throws IOException;
 
     Stat getattr(Inode inode) throws IOException;
 
@@ -66,4 +67,44 @@ public interface VirtualFileSystem {
     boolean hasIOLayout(Inode inode) throws IOException;
 
     AclCheckable getAclCheckable();
+
+    public static class WriteResult {
+        private final int bytesWritten;
+        private final StabilityLevel stabilityLevel;
+
+        public WriteResult(StabilityLevel stabilityLevel, int bytesWritten) {
+            this.stabilityLevel = stabilityLevel;
+            this.bytesWritten = bytesWritten;
+        }
+
+        public int getBytesWritten() {
+            return bytesWritten;
+        }
+
+        public StabilityLevel getStabilityLevel() {
+            return stabilityLevel;
+        }
+    }
+
+    //NOTE - stability values and ordinals are the same for nfs 3 and 4
+    public static enum StabilityLevel {
+        UNSTABLE, DATA_SYNC, FILE_SYNC;
+
+        public static StabilityLevel fromStableHow(int stableHowValue) {
+            switch (stableHowValue) {
+                case stable_how4.UNSTABLE4:
+                    return UNSTABLE;
+                case stable_how4.DATA_SYNC4:
+                    return DATA_SYNC;
+                case stable_how4.FILE_SYNC4:
+                    return FILE_SYNC;
+                default:
+                    throw new IllegalArgumentException("unhandled stability value "+stableHowValue);
+            }
+        }
+
+        public int toStableHow() {
+            return ordinal();
+        }
+    }
 }

commit 075343dec86f344f2ff07bb20cfcf20d5763b290

nfs4: fix number of written bytes on WRITE
fixes: 6dc099b
Acked-by: Paul Millar
Target: master, 0.10

core/src/main/java/org/dcache/nfs/v4/OperationWRITE.java
@@ -87,7 +87,7 @@ public class OperationWRITE extends AbstractNFSv4Operation {
 
         res.status = nfsstat.NFS_OK;
         res.resok4 = new WRITE4resok();
-        res.resok4.count = new count4(writeResult.getStabilityLevel().toStableHow());
+        res.resok4.count = new count4(writeResult.getBytesWritten());
         res.resok4.committed = stable_how4.FILE_SYNC4;
         res.resok4.writeverf = new verifier4();
         res.resok4.writeverf.value = new byte[nfs4_prot.NFS4_VERIFIER_SIZE];