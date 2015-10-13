dCache/nfs4j

commit 2ea7c2229b79dcfc18f6569bc36905656d60c85b

nfs4: always promise device notifications
device notifications used by server to inform a client
that device is removed or points to a new address.

The linux kernel client >= 4.0 will use device address
information only once and make an extra requests to
discover data server address. This leads to bad performance.

We can promise to notify client about device id -> address
change, even if it's not the case. Client can free unused
information anyway.

Acked-by: Gerd Behrmann
Target: master

core/src/main/java/org/dcache/nfs/v4/OperationGETDEVICEINFO.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2009 - 2012 Deutsches Elektronen-Synchroton,
+ * Copyright (c) 2009 - 2015 Deutsches Elektronen-Synchroton,
  * Member of the Helmholtz Association, (DESY), HAMBURG, GERMANY
  *
  * This library is free software; you can redistribute it and/or modify
@@ -31,8 +31,11 @@ import org.dcache.nfs.v4.xdr.device_addr4;
 import java.io.IOException;
 import org.dcache.nfs.nfsstat;
 import org.dcache.nfs.status.InvalException;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import static org.dcache.nfs.v4.xdr.notify_deviceid_type4.*;
 
 public class OperationGETDEVICEINFO extends AbstractNFSv4Operation {
 
@@ -67,8 +70,7 @@ public class OperationGETDEVICEINFO extends AbstractNFSv4Operation {
 
         res.gdir_resok4.gdir_device_addr = deviceInfo;
         res.gdir_resok4.gdir_device_addr.da_layout_type = layouttype4.LAYOUT4_NFSV4_1_FILES;
-        res.gdir_resok4.gdir_notification = new bitmap4();
-        res.gdir_resok4.gdir_notification.value = new int[] {0};
+        res.gdir_resok4.gdir_notification = bitmap4.of(NOTIFY_DEVICEID4_CHANGE, NOTIFY_DEVICEID4_DELETE);
 
         res.gdir_status = nfsstat.NFS_OK;
     }

commit b19589df5fdaf3fa4201203a202db44b0f7de8b2

nfs4: do not indicate device notification if client don't expect them
fix regression introduced in 2ea7c22

On GETDEVICEINFO, client provides notify_types bitmap for mapping
notification it is interested in receiving. If the bitmap is zero
or empty, then the device ID's notifications are turned off.

We always return a non ampty bitmap independent from client's requests,
which is:

  - spec violation
  - RHEL(SL) 6 client rejects such devices

Ackek-by: Paul Millar
Target: master, 0.11

core/src/main/java/org/dcache/nfs/v4/OperationGETDEVICEINFO.java
@@ -66,7 +66,16 @@ public class OperationGETDEVICEINFO extends AbstractNFSv4Operation {
         }
 
         res.gdir_resok4.gdir_device_addr = deviceInfo;
-        res.gdir_resok4.gdir_notification = bitmap4.of(NOTIFY_DEVICEID4_CHANGE, NOTIFY_DEVICEID4_DELETE);
+        res.gdir_resok4.gdir_notification = new bitmap4();
+        /*
+         * provide faked notification only if client expects them
+         */
+        if (_args.opgetdeviceinfo.gdia_notify_types.isSet(NOTIFY_DEVICEID4_CHANGE)) {
+            res.gdir_resok4.gdir_notification.set(NOTIFY_DEVICEID4_CHANGE);
+        }
+        if (_args.opgetdeviceinfo.gdia_notify_types.isSet(NOTIFY_DEVICEID4_DELETE)) {
+            res.gdir_resok4.gdir_notification.set(NOTIFY_DEVICEID4_DELETE);
+        }
 
         res.gdir_status = nfsstat.NFS_OK;
     }