151547fd75f25ad1ebb8b7a7827de193ecc3fe94
Tigran Mkrtchyan <tigran.mkrtchyan@desy.de>
2017-04-12 11:36:20 +0200

    add move protection handling

--- core/src/main/java/org/dcache/nfs/vfs/ChimeraVfs.java
+++ core/src/main/java/org/dcache/nfs/vfs/ChimeraVfs.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2009 - 2015 Deutsches Elektronen-Synchroton,
+ * Copyright (c) 2009 - 2017 Deutsches Elektronen-Synchroton,
  * Member of the Helmholtz Association, (DESY), HAMBURG, GERMANY
  *
  * This library is free software; you can redistribute it and/or modify
@@ -44,6 +44,7 @@ import org.dcache.chimera.InvalidArgumentChimeraException;
 import org.dcache.chimera.IsDirChimeraException;
 import org.dcache.chimera.JdbcFs;
 import org.dcache.chimera.NotDirChimeraException;
+import org.dcache.chimera.PermissionDeniedChimeraFsException;
 import org.dcache.chimera.StorageGenericLocation;
 import org.dcache.nfs.status.*;
 import org.dcache.nfs.v4.NfsIdMapping;
@@ -153,6 +154,8 @@ public class ChimeraVfs implements VirtualFileSystem, AclCheckable {
         FsInode to = toFsInode(dest);
 	try {
 	    return _fs.move(from, oldName, to, newName);
+        } catch (PermissionDeniedChimeraFsException e) {
+            throw new PermException(e.getMessage(), e);
 	} catch (NotDirChimeraException e) {
 	    throw new NotDirException("not a directory");
 	} catch (FileExistsChimeraFsException e) {
