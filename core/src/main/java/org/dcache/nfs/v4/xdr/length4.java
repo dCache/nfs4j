ef6a28b4e348a5ee0e65477e253bf5351b9194e1
Tigran Mkrtchyan <tigran.mkrtchyan@desy.de>
2017-09-05 14:43:55 +0200

    nfs4: add toString methods to length4, uint64_6 and settime4 classes
    Motivation:
    glorify log files
    
    Modification:
    add missing toString methods
    
    Result:
    cleaner logging
    
    Acked-by: Paul Millar
    Target: master, 0.14, 0.15
    (cherry picked from commit a150f9c0aa603b98131b2700974bb2b788de5a6f)
    Signed-off-by: Tigran Mkrtchyan <tigran.mkrtchyan@desy.de>

--- core/src/main/java/org/dcache/nfs/v4/xdr/length4.java
+++ core/src/main/java/org/dcache/nfs/v4/xdr/length4.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2009 - 2012 Deutsches Elektronen-Synchroton,
+ * Copyright (c) 2009 - 2017 Deutsches Elektronen-Synchroton,
  * Member of the Helmholtz Association, (DESY), HAMBURG, GERMANY
  *
  * This library is free software; you can redistribute it and/or modify
@@ -33,6 +33,11 @@ public class length4 extends uint64_t {
     public length4(XdrDecodingStream xdr)
            throws OncRpcException, IOException {
         super(xdr);
+    }
+
+    @Override
+    public String toString() {
+        return Long.toString(value);
     }
 
 }
