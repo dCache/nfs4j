74b0c0d124f9f6d0b1856f1698682ef22c0e9bd6
Tigran Mkrtchyan <tigran.mkrtchyan@desy.de>
2018-08-14 22:40:36 +0200

    test: refactor READDIR unit test to reduce code duplication
    DRY
    
    Acked-by: Paul Millar
    Target: master

--- core/src/test/java/org/dcache/nfs/v4/OperationREADDIRTest.java
+++ core/src/test/java/org/dcache/nfs/v4/OperationREADDIRTest.java
@@ -1,8 +1,11 @@
 package org.dcache.nfs.v4;
 
+import com.google.common.primitives.Ints;
+import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.IntStream;
 import org.dcache.nfs.nfsstat;
 import org.dcache.nfs.status.MovedException;
 import org.dcache.nfs.status.TooSmallException;
@@ -22,13 +25,10 @@ import org.dcache.nfs.vfs.Stat;
 import org.dcache.nfs.vfs.VirtualFileSystem;
 import org.dcache.oncrpc4j.xdr.Xdr;
 import org.junit.Test;
-import org.mockito.Mockito;
 import org.junit.Before;
 
 import static org.dcache.nfs.v4.NfsTestUtils.generateRpcCall;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
 public class OperationREADDIRTest {
@@ -40,9 +40,17 @@ public class OperationREADDIRTest {
     private nfs_resop4 result;
     private CompoundContext context;
 
+    private int ino = 1; // first allowed cookie
+    private long startCookie;
+    private long verifier;
+    private int maxCount = 1024;
+    private int dirCount = 512;
+
+    private entry4 entries;
+
     @Before
     public void setup() throws Exception {
-        dirHandle = new FileHandle(0, 1, 0, new byte[]{0, 0, 0, 1}); // the dir we want to read
+        dirHandle = new FileHandle(0, 0, 0, new byte[]{0, 0, 0, 0}); // the dir we want to read
         dirInode = new Inode(dirHandle);
         dirStat = new Stat(); // the stat marking it as a dir
         //noinspection OctalInteger
@@ -57,8 +65,8 @@ public class OperationREADDIRTest {
         dirStat.setDev(1);
         dirStat.setFileid(1);
         dirStat.setSize(512);
-        vfs = Mockito.mock(VirtualFileSystem.class); // the vfs serving it
-        Mockito.when(vfs.getattr(Mockito.eq(dirInode))).thenReturn(dirStat);
+        vfs = mock(VirtualFileSystem.class); // the vfs serving it
+        when(vfs.getattr(eq(dirInode))).thenReturn(dirStat);
 
         result = nfs_resop4.resopFor(nfs_opnum4.OP_READDIR);
         context = new CompoundContextBuilder()
@@ -70,154 +78,163 @@ public class OperationREADDIRTest {
 
     @Test
     public void testReadDirWithNoResults() throws Exception {
-
-        verifier4 cookieVerifier = verifier4.valueOf(0);
-
-        // vfs will return an empty list from the vfs for dir (technically legal)
-        Mockito.when(vfs.list(eq(dirInode), anyObject(), anyLong())).thenReturn(new DirectoryStream(cookieVerifier.value, Collections.emptyList()));
-
-        nfs_argop4 op = new CompoundBuilder()
-                .withReaddir(0, cookieVerifier, 1024, 512)
-                .build().argarray[0];
-
-        OperationREADDIR readdirOp = new OperationREADDIR(op);
-        readdirOp.process(context, result);
+        givenDirectory();
+        listed();
     }
 
     @Test(expected = TooSmallException.class)
     public void testReadDirWithTinyLimit() throws Exception {
-
-        verifier4 cookieVerifier = verifier4.valueOf(0);
-
-        // vfs will return only "." and ".." as contents, both leading to itself
-        List<DirectoryEntry> dirContents = new ArrayList<>();
-        dirContents.add(new DirectoryEntry(".", dirInode, dirStat, 1));
-        dirContents.add(new DirectoryEntry("..", dirInode, dirStat, 2));
-        Mockito.when(vfs.list(eq(dirInode), anyObject(), anyLong())).thenReturn(new DirectoryStream(cookieVerifier.value, dirContents));
-
-        nfs_argop4 op = new CompoundBuilder()
-                .withReaddir(0, cookieVerifier, 1024, 10)
-                .build()
-                .argarray[0];
-
-        OperationREADDIR readdirOp = new OperationREADDIR(op);
-        readdirOp.process(context, result);
+        givenDirectory();
+        withMaxRpcReplySize(10);
+        listed();
     }
 
     @Test
     public void testSemiValidVerifier() throws Exception {
 
-        verifier4 cookieVerifier = verifier4.valueOf(dirStat.getGeneration());
-
-        // vfs will return only "." and ".." as contents, both leading to itself
-        List<DirectoryEntry> dirContents = new ArrayList<>();
-        dirContents.add(new DirectoryEntry(".", dirInode, dirStat, 1));
-        dirContents.add(new DirectoryEntry("..", dirInode, dirStat, 2));
-        dirContents.add(new DirectoryEntry("file", dirInode, dirStat,3));
-        Mockito.when(vfs.list(eq(dirInode), anyObject(), anyLong())).thenReturn(new DirectoryStream(cookieVerifier.value, dirContents));
-
+        givenDirectory(file("file"));
         long cookie = 4; // 3 is the first allowed non zero value
-
-        nfs_argop4 op = new CompoundBuilder()
-                .withReaddir(cookie, cookieVerifier, 1024, 512)
-                .build().argarray[0];
-
-        OperationREADDIR readdirOp = new OperationREADDIR(op);
-        readdirOp.process(context, result);
+        fromCookie(cookie);
+        listed();
     }
 
     @Test
     public void testEofIfLastEntryDoesNotFit() throws Exception {
 
-        verifier4 cookieVerifier = new verifier4(DirectoryStream.ZERO_VERIFIER);
+        DirectoryEntry[] dirEntries = IntStream.range(0, 21)
+                .mapToObj(i -> "file" + i)
+                .map(this::file)
+                .toArray(DirectoryEntry[]::new);
 
-        // vfs will return only "." and ".." as contents, both leading to itself
-        List<DirectoryEntry> dirContents = new ArrayList<>();
-        dirContents.add(new DirectoryEntry(".", dirInode, dirStat, 1));
-        dirContents.add(new DirectoryEntry("..", dirInode, dirStat, 2));
+        givenDirectory(dirEntries);
+        withMaxRpcReplySize(1000); // only 20 will fit
 
-        for (int i = 0; i < 21; i++) {
-            dirContents.add(new DirectoryEntry("file" + i, dirInode, dirStat, 3 + i));
-        }
+        listed();
 
-        Mockito.when(vfs.list(eq(dirInode), anyObject(), anyLong())).thenReturn(new DirectoryStream(cookieVerifier.value, dirContents));
-
-        nfs_argop4 op = new CompoundBuilder()
-                .withReaddir(0, cookieVerifier, 1024, 1000) // only 20 entry will fit
-                .build().argarray[0];
-
-        OperationREADDIR readdirOp = new OperationREADDIR(op);
-        readdirOp.process(context, result);
-
-        int n = 0;
-
-        entry4 entry = result.opreaddir.resok4.reply.entries;
-        while(entry != null) {
-            entry = entry.nextentry;
-            n++;
-        }
-
-        assertEquals("Not all entries returned", dirContents.size() - 3, n);
+        assertEquals("Not all entries returned", dirEntries.length - 1, entryCount());
         assertFalse("The last entry is missed", result.opreaddir.resok4.reply.eof);
     }
 
     @Test(expected = MovedException.class)
     public void testReaddirErrorAttributeFullOp() throws Exception {
 
-        verifier4 cookieVerifier = new verifier4(DirectoryStream.ZERO_VERIFIER);
+        givenDirectory(file("file"));
+        when(vfs.getAcl(any(Inode.class))).thenThrow(MovedException.class);
 
-        // vfs will return only "." and ".." as contents, both leading to itself
-        List<DirectoryEntry> dirContents = new ArrayList<>();
-        dirContents.add(new DirectoryEntry(".", dirInode, dirStat, 1));
-        dirContents.add(new DirectoryEntry("..", dirInode, dirStat, 2));
-
-        Inode fileInode = mock(Inode.class);
-        // we use gertAcl methos as it's the only one which throws checked exception
-        Mockito.when(vfs.getAcl(fileInode)).thenThrow(MovedException.class);
-
-        dirContents.add(new DirectoryEntry("file", fileInode, dirStat, 3));
-
-
-        Mockito.when(vfs.list(eq(dirInode), anyObject(), anyLong())).thenReturn(new DirectoryStream(cookieVerifier.value, dirContents));
-
-        nfs_argop4 op = new CompoundBuilder()
-                .withReaddirWithAttrs(0, cookieVerifier, 1024, 1000, nfs4_prot.FATTR4_ACL)
-                .build().argarray[0];
-
-        OperationREADDIR readdirOp = new OperationREADDIR(op);
-        readdirOp.process(context, result);
+        listed(nfs4_prot.FATTR4_ACL);
     }
 
     @Test
     public void testReaddirErrorAttributeSingleEntry() throws Exception {
 
-        verifier4 cookieVerifier = new verifier4(DirectoryStream.ZERO_VERIFIER);
+        givenDirectory(file("file"));
+        when(vfs.getAcl(any(Inode.class))).thenThrow(new MovedException()); // can't use MovedException.class as status value not initialized
+
+        listed(nfs4_prot.FATTR4_ACL, nfs4_prot.FATTR4_RDATTR_ERROR);
+
+        fattr4_rdattr_error rderror = new fattr4_rdattr_error(new Xdr(entries.attrs.attr_vals.value));
+
+        assertTrue("rdattr_error is not set", entries.attrs.attrmask.isSet(nfs4_prot.FATTR4_RDATTR_ERROR));
+        assertEquals("Invalid error code returned", nfsstat.NFSERR_MOVED, rderror.value);
+    }
+
+    private DirectoryEntry dir(String name) {
+
+        int cookie = ino++;
+
+        FileHandle handle = new FileHandle(0, 1, 0, Ints.toByteArray(cookie));
+        Inode inode = new Inode(handle);
+        Stat stat = new Stat(); // the stat marking it as a dir
+        //noinspection OctalInteger
+        stat.setMode(Stat.S_IFDIR | 0755);
+        stat.setMTime(System.currentTimeMillis());
+        stat.setATime(System.currentTimeMillis());
+        stat.setCTime(System.currentTimeMillis());
+        stat.setGeneration(1);
+        stat.setNlink(2);
+        stat.setUid(1);
+        stat.setGid(2);
+        stat.setDev(1);
+        stat.setFileid(cookie);
+        stat.setSize(512);
+
+        try {
+            when(vfs.getattr(eq(inode))).thenReturn(stat);
+        } catch (IOException e) {
+            fail();
+        }
+        return new DirectoryEntry(name, inode, stat, cookie);
+    }
+
+    private DirectoryEntry file(String name) {
+
+        int cookie = ino++;
+
+        FileHandle handle = new FileHandle(0, 1, 0, Ints.toByteArray(cookie));
+        Inode inode = new Inode(handle);
+        Stat stat = new Stat(); // the stat marking it as a dir
+        //noinspection OctalInteger
+        stat.setMode(Stat.S_IFREG | 0644);
+        stat.setMTime(System.currentTimeMillis());
+        stat.setATime(System.currentTimeMillis());
+        stat.setCTime(System.currentTimeMillis());
+        stat.setGeneration(1);
+        stat.setNlink(2);
+        stat.setUid(1);
+        stat.setGid(2);
+        stat.setDev(1);
+        stat.setFileid(cookie);
+        stat.setSize(1024);
+
+        try {
+            when(vfs.getattr(eq(inode))).thenReturn(stat);
+        } catch (IOException e) {
+            fail();
+        }
+        return new DirectoryEntry(name, inode, stat, cookie);
+    }
+
+    private void givenDirectory(DirectoryEntry... enties) throws IOException {
+
+          verifier4 cookieVerifier = verifier4.valueOf(1);
 
         // vfs will return only "." and ".." as contents, both leading to itself
         List<DirectoryEntry> dirContents = new ArrayList<>();
-        dirContents.add(new DirectoryEntry(".", dirInode, dirStat, 1));
-        dirContents.add(new DirectoryEntry("..", dirInode, dirStat, 2));
+        dirContents.add(dir("."));
+        dirContents.add(dir(".."));
+        dirContents.addAll(Arrays.asList(enties));
+        when(vfs.list(eq(dirInode), anyObject(), anyLong())).thenReturn(new DirectoryStream(cookieVerifier.value, dirContents));
+    }
 
-        Inode fileInode = mock(Inode.class);
-        // we use gertAcl methos as it's the only one which throws checked exception
-        Mockito.when(vfs.getAcl(fileInode)).thenThrow(new MovedException()); // can't use MovedException.class as status value not initialized
+    public void fromCookie(long start) {
+        startCookie = start;
+    }
 
-        dirContents.add(new DirectoryEntry("file", fileInode, dirStat, 3));
+    public void withMaxDirReplySize(int size) {
+        dirCount = size;
+    }
 
-        Mockito.when(vfs.list(eq(dirInode), anyObject(), anyLong())).thenReturn(new DirectoryStream(cookieVerifier.value, dirContents));
+    public void withMaxRpcReplySize(int size) {
+        maxCount = size;
+    }
 
+    public void listed(int... attrs) throws IOException {
         nfs_argop4 op = new CompoundBuilder()
-                .withReaddirWithAttrs(0, cookieVerifier, 1024, 1000,
-                        nfs4_prot.FATTR4_ACL, nfs4_prot.FATTR4_RDATTR_ERROR)
+                .withReaddir(startCookie, verifier4.valueOf(verifier), dirCount, maxCount, attrs)
                 .build().argarray[0];
 
         OperationREADDIR readdirOp = new OperationREADDIR(op);
         readdirOp.process(context, result);
 
-        entry4 entry = result.opreaddir.resok4.reply.entries;
-        fattr4_rdattr_error rderror = new fattr4_rdattr_error(new Xdr(entry.attrs.attr_vals.value));
+        entries = result.opreaddir.resok4.reply.entries;
+    }
 
-        assertTrue("rdattr_error is not set", entry.attrs.attrmask.isSet(nfs4_prot.FATTR4_RDATTR_ERROR));
-        assertEquals("Invalid error code returned", nfsstat.NFSERR_MOVED, rderror.value);
+    private int entryCount() {
+        int n = 0;
+        while (entries != null) {
+            entries = entries.nextentry;
+            n++;
+        }
+        return n;
     }
 }
