0ce8d2a4f086c77d0bf80e0d52c038975f438f12
Tigran Mkrtchyan <tigran.mkrtchyan@desy.de>
2024-03-05 17:54:00 +0100

    vfs: make file handle immutable class
    no reasons not to.
    
    Acked-by: Marina Sahakyan
    Target: master

--- core/src/main/java/org/dcache/nfs/vfs/Inode.java
+++ core/src/main/java/org/dcache/nfs/vfs/Inode.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2009 - 2019 Deutsches Elektronen-Synchroton,
+ * Copyright (c) 2009 - 2024 Deutsches Elektronen-Synchroton,
  * Member of the Helmholtz Association, (DESY), HAMBURG, GERMANY
  *
  * This library is free software; you can redistribute it and/or modify
@@ -23,7 +23,7 @@ import java.util.Arrays;
 
 public class Inode {
 
-    FileHandle fh;
+    private final FileHandle fh;
 
     public Inode(byte[] bytes) {
         this(new FileHandle(bytes));
