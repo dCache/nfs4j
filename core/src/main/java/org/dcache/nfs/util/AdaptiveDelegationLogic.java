e1e164e754320a4393cd2c74e3031f34fdd2bc3a
Tigran Mkrtchyan <tigran.mkrtchyan@desy.de>
2025-04-18 23:49:13 +0200

    nfs4: add heuristic to issue delegation based on file usage
    Motivation:
    Current logic issues delegation if there is already exist an open on the
    file. However, the loop like OPEN+READ+CLOSE won't be detected.
    
    Modification:
    Introduce Adaptive Delegation Logic that uses two queues for delegation
    heuristic.  The algorithm works as follows:
    
       1. When a file is accessed for the first time, it is added to the Eviction Queue
       2. If a file in the Eviction Queue is accessed again, it is moved to the Active Queue
       3. If a file in the Active Queue is accessed again, the system recommends delegation
       4. If a file in the Active Queue is not accessed for a specified idle time, it is moved
          to the Eviction Queue
       5. When active queue reaches capacity, the least recently used (LRU) file is pushed into
          eviction queue
       6. When evicted from active queue entry is not accessed for a specified idle time, entry evicted
       7. When eviction queue reaches capacity, the least recently used (LRU) file is evicted
    
    Result:
    open-read-close loops are detected and subject for delegation
    
    Acked-by: Paul Millar
    Target: master

---
+++ core/src/main/java/org/dcache/nfs/util/AdaptiveDelegationLogic.java
@@ -0,0 +1,315 @@
+/*
+ * Copyright (c) 2025 Deutsches Elektronen-Synchroton,
+ * Member of the Helmholtz Association, (DESY), HAMBURG, GERMANY
+ *
+ * This library is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Library General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this program (see the file COPYING.LIB for more
+ * details); if not, write to the Free Software Foundation, Inc.,
+ * 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+package org.dcache.nfs.util;
+
+
+import com.google.common.annotations.VisibleForTesting;
+import org.dcache.nfs.v4.NFS4Client;
+import org.dcache.nfs.v4.xdr.clientid4;
+import org.dcache.nfs.vfs.Inode;
+
+import java.time.Clock;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * <pre>
+///
+///  Adaptive Delegation Logic for NFSv4 file delegation heuristic.
+///
+///  This system uses two queues to track file access patterns:
+///    - Eviction Queue: Tracks files that have been accessed only once
+///    - Active Queue: Contains files that have been accessed multiple times
+///
+///
+///  The algorithm works as follows:
+///
+///    1. When a file is accessed for the first time, it is added to the Eviction Queue
+///    2. If a file in the Eviction Queue is accessed again, it is moved to the Active Queue
+///    3. If a file in the Active Queue is accessed again, the system recommends delegation
+///    4. If a file in the Active Queue is not accessed for a specified idle time, it is moved to the Eviction Queue
+///    5. When active queue reaches capacity, the least recently used (LRU) file is pushed into eviction queue
+///    6. When evicted from active queue entry is not accessed for a specified idle time, entry evicted
+///    7. When eviction queue reaches capacity, the least recently used (LRU) file is evicted
+///
+///
+///  Uses LinkedHashMap to maintain insertion order and LRU eviction policy.
+///
+///
+///
+///                                                  +---------------+
+///                                                  |  File Access  |
+///                                                  +-------+-------+
+///                                                          |
+///                                                          v
+///                                                 +--------------------+
+///                                                 | Is in Active Queue?|
+///                                                 +--------+-----------+
+///                                                          |
+///                                      +-------------------+-------------------+
+///                                      | No                                Yes |
+///                                      |                                       |
+///                                      v                                       v
+///                          +---------------------+                   +---------------------+
+///                          |Is in Eviction Queue?|                   | is Idletime Excided?|
+///                          +---------+-----------+                   +---------------------+
+///                                    |                                          |
+///                     +--------------|---------------+              +----------------------+
+///                     |                              |              | Yes               No |
+///                     | Yes                          | No           |                      |
+///                     v                              v              |                      V
+///         +--------------------+          +----------------------+  |             +-------------------+
+///         |Move to Active Queue|          |Add to Eviction Queue | <|             | Offer Delegation  |
+///         +--------------------+          +----------+-----------+                +-------------------+
+///                     |                              |
+///                     v                              v
+///             +---------------+              +---------------+
+///             |Is A.Q. Full?  |              |Is E.Q. Full?  |
+///             +-------+-------+              +-------+-------+
+///                     |                              |
+///          +----------+----------+        +----------+----------+
+///          |                     |  No    |                     |
+///          | Yes                 +--------+                Yes  |
+///          v                          |                         v
+///  +---------------+                  v                 +---------------+
+///  |LRU  to E.Q.   |             +--------+             |LRU from E.Q.  |
+///  +---------------+             |  Noop  |             +---------------+
+///                                +--------+
+///
+///
+ </pre>
+*/
+
+public class AdaptiveDelegationLogic {
+
+
+    /**
+     * Per NFS client queues for file delegation.
+     */
+    private static class ClientQueues {
+        /**
+         * Eviction Queue for files accessed once.
+         */
+        final LinkedHashMap<Inode, Instant> evictionQueue;
+
+        /**
+         * Active Queue for files accessed multiple times.
+         */
+        final LinkedHashMap<Inode, Instant> activeQueue;
+
+        /**
+         * Lock for synchronizing access to the queues.
+         */
+        private final Lock lock = new ReentrantLock();
+
+        ClientQueues(int maxEvictionQueueSize, int maxActiveQueueSize) {
+            // Initialize eviction queue with LRU eviction policy
+            this.evictionQueue = new LinkedHashMap<>(maxEvictionQueueSize, 0.75f, true) {
+                @Override
+                protected boolean removeEldestEntry(Map.Entry<Inode, Instant> eldest) {
+                    return size() > maxEvictionQueueSize;
+                }
+            };
+
+            // Initialize active queue with LRU eviction policy
+            this.activeQueue = new LinkedHashMap<>(maxActiveQueueSize, 0.75f, true);
+        }
+
+        void lock() {
+            lock.lock();
+        }
+
+        void unlock() {
+            lock.unlock();
+        }
+    }
+
+
+    /**
+     * Maximum size of eviction queue.
+     */
+    private final int maxEvictionQueueSize;
+
+    /**
+     * Maximum size of active queue.
+     */
+    private final int maxActiveQueueSize;
+
+    /**
+     * Per-client queue map.
+     */
+    private final Map<clientid4, ClientQueues> clientQueuesMap = new ConcurrentHashMap<>();
+
+    /**
+     * Maximum idle time for files in the active queue.
+     * Files that exceed this time will be discarded by-passing eviction queue.
+     */
+    private final Duration maxIdleTime;
+
+
+    /**
+     * Time source for generating timestamps.
+     * This allows for easier testing and mocking of time-related functionality.
+     */
+    private final Clock clock;
+
+    /**
+     * Creates a new instance of DelegationQueueSystem
+     *
+     * @param maxActiveQueueSize   Maximum size of active queue.
+     * @param maxEvictionQueueSize Maximum size of eviction queue.
+     * @param maxIdleTime Maximum idle time for files in the active queue.
+     */
+    public AdaptiveDelegationLogic(int maxActiveQueueSize, int maxEvictionQueueSize, Duration maxIdleTime) {
+        this(maxActiveQueueSize, maxEvictionQueueSize, maxIdleTime, Clock.systemDefaultZone());
+    }
+
+    /**
+     * Creates a new instance of DelegationQueueSystem. For internal use only.
+     *
+     * @param maxActiveQueueSize   Maximum size of active queue.
+     * @param maxEvictionQueueSize Maximum size of eviction queue.
+     * @param maxIdleTime Maximum idle time for files in the active queue.
+     * @param clock Clock to use for timestamp generation, for testing purposes.
+     */
+    @VisibleForTesting
+    AdaptiveDelegationLogic(int maxActiveQueueSize, int maxEvictionQueueSize, Duration maxIdleTime, Clock clock) {
+        this.maxActiveQueueSize = maxActiveQueueSize;
+        this.maxEvictionQueueSize = maxEvictionQueueSize;
+        this.maxIdleTime = maxIdleTime;
+        this.clock = clock;
+    }
+
+    /**
+     * Get client queues, creating them if they don't exist
+     *
+     * @param client NFS client to get queues for
+     * @return ClientQueues for the specified client
+     */
+    private ClientQueues getClientQueues(NFS4Client client) {
+        return clientQueuesMap.computeIfAbsent(
+                client.getId(),
+                k -> new ClientQueues(maxEvictionQueueSize, maxActiveQueueSize)
+        );
+    }
+
+    /**
+     * Check if a file should be delegated based on access frequency.
+     *
+     * @param client The NFS client making the request
+     * @param inode The file to check
+     * @return true if the file should be delegated, false otherwise
+     */
+    public boolean shouldDelegate(NFS4Client client, Inode inode) {
+        var clientQueues = getClientQueues(client);
+        var currentTime = Instant.now(clock);
+
+        clientQueues.lock();
+        try {
+            // Case 1: File is in Active Queue - offer delegation
+            var lastAccessTime = clientQueues.activeQueue.get(inode);
+            if (lastAccessTime != null) {
+
+                // Check if the file has been idle for too long
+                if (lastAccessTime.plus(maxIdleTime).isBefore(currentTime)) {
+                    // Move to Eviction Queue
+                    clientQueues.evictionQueue.put(inode, lastAccessTime);
+                    clientQueues.activeQueue.remove(inode);
+                    return false;
+                }
+
+                // Update access time
+                clientQueues.activeQueue.put(inode, currentTime);
+                return true;
+            }
+
+            // Case 2: File is in Eviction Queue - move to Active Queue
+            if (clientQueues.evictionQueue.containsKey(inode)) {
+                // Remove from Eviction Queue
+                clientQueues.evictionQueue.remove(inode);
+                // Add to Active Queue
+                clientQueues.activeQueue.put(inode, currentTime);
+
+                // Move the least recently used file from Active Queue into Eviction Queue or drop, if too old
+                if (clientQueues.activeQueue.size() > maxActiveQueueSize) {
+
+                    var eldestEntry = clientQueues.activeQueue.entrySet().iterator().next();
+                    if (eldestEntry.getValue().plus(maxIdleTime).isAfter(currentTime)) {
+                        clientQueues.evictionQueue.put(eldestEntry.getKey(), eldestEntry.getValue());
+                    }
+
+                    clientQueues.activeQueue.remove(eldestEntry.getKey());
+                }
+                return true;
+            }
+
+            // Case 3: File is not in any queue - add to Eviction Queue
+            clientQueues.evictionQueue.put(inode, currentTime);
+            return false;
+        } finally {
+            clientQueues.unlock();
+        }
+    }
+
+    /**
+     * Check if a file is in the active queue
+     *
+     * @param client The NFS client making the request
+     * @param inode The file to check
+     * @return true if the file is in the active queue, false otherwise
+     */
+    @VisibleForTesting
+    synchronized boolean isInActive(NFS4Client client, Inode inode) {
+        return getClientQueues(client).activeQueue.containsKey(inode);
+    }
+
+    /**
+     * Check if a file is in the eviction queue
+     *
+     * @param client The NFS client making the request
+     * @param inode The file to check
+     * @return true if the file is in the eviction queue, false otherwise
+     */
+    @VisibleForTesting
+    synchronized boolean isInEvictionQueue(NFS4Client client, Inode inode) {
+        return getClientQueues(client).evictionQueue.containsKey(inode);
+    }
+
+    /**
+     * Reset queues for a specific client
+     *
+     * @param client The NFS client whose queues should be reset
+     */
+    public synchronized void reset(NFS4Client client) {
+        clientQueuesMap.remove(client.getId());
+    }
+
+    /**
+     * Reset all queues
+     */
+    public void reset() {
+        clientQueuesMap.clear();
+    }
+}
---
+++ core/src/test/java/org/dcache/nfs/util/AdaptiveDelegationLogicTest.java
@@ -0,0 +1,153 @@
+package org.dcache.nfs.util;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import org.dcache.nfs.v4.NFS4Client;
+import org.dcache.nfs.v4.xdr.clientid4;
+import org.dcache.nfs.vfs.Inode;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.time.Duration;
+
+public class AdaptiveDelegationLogicTest {
+
+    public static final int EVICTION_QUEUE_CAPACITY = 53;
+    public static final int ACTIVE_QUEUE_CAPACITY = 17;
+
+    private AdaptiveDelegationLogic logic;
+    private ManualClock clock;
+    private NFS4Client client1;
+    private NFS4Client client2;
+
+    @Before
+    public void setUp() {
+        client1 = mock(NFS4Client.class);
+        when(client1.getId()).thenReturn(new clientid4(1L));
+        client2 = mock(NFS4Client.class);
+        when(client2.getId()).thenReturn(new clientid4(2L));
+
+        clock = new ManualClock();
+        logic = new AdaptiveDelegationLogic(ACTIVE_QUEUE_CAPACITY, EVICTION_QUEUE_CAPACITY, Duration.ofSeconds(10), clock);
+    }
+
+    @Test
+    public void shouldDelegateReturnsFalseForFirstTime() {
+        Inode inode = Inode.forFile("file1".getBytes());
+        assertFalse("File in eviction queue should not be delegated", logic.shouldDelegate(client1, inode));
+    }
+
+    @Test
+    public void shouldDelegateReturnsTrueForActiveQueueFile() {
+        Inode inode = Inode.forFile("file1".getBytes());
+        logic.shouldDelegate(client1, inode); // Add to eviction queue
+        assertTrue("File in active queue should be delegated", logic.shouldDelegate(client1, inode));
+    }
+
+    @Test
+    public void isActiveReturnsTrueForActiveQueueFile() {
+        Inode inode = Inode.forFile("file4".getBytes());
+        logic.shouldDelegate(client1, inode); // Add to eviction queue
+        boolean delegate = logic.shouldDelegate(client1, inode); // Move to active queue, delegate
+        assertTrue("File in active queue should be active", logic.isInActive(client1, inode));
+        assertTrue("File in active queue suggest delegation", delegate);
+    }
+
+    @Test
+    public void isActiveReturnsFalseForEvictionQueueFile() {
+        Inode inode = Inode.forFile("file5".getBytes());
+        logic.shouldDelegate(client1, inode); // Add to eviction queue
+        assertFalse("File in eviction queue should not be active", logic.isInActive(client1, inode));
+    }
+
+    @Test
+    public void isInEvictionQueueReturnsTrueForEvictionQueueFile() {
+        Inode inode = Inode.forFile("file6".getBytes());
+        logic.shouldDelegate(client1, inode); // Add to eviction queue
+        assertTrue("File in eviction queue should be detected", logic.isInEvictionQueue(client1, inode));
+    }
+
+    @Test
+    public void isInEvictionQueueReturnsFalseForActiveQueueFile() {
+        Inode inode = Inode.forFile("file7".getBytes());
+        logic.shouldDelegate(client1, inode); // Add to eviction queue
+        logic.shouldDelegate(client1, inode); // Move to active queue, delegate
+        assertFalse("File in active queue should not be in eviction queue", logic.isInEvictionQueue(client1, inode));
+    }
+
+    @Test
+    public void activeQueueEvictsLeastRecentlyUsedFileToEvictionQueue() {
+        Inode inode1 = Inode.forFile("file".getBytes());
+
+        logic.shouldDelegate(client1, inode1); // Add to eviction queue
+
+        // trigger eviction from active queue
+        for (int i = 0; i < ACTIVE_QUEUE_CAPACITY; i++) {
+            // call twice to trigger move to active queue
+            logic.shouldDelegate(client1, Inode.forFile(("file" + (i + 10)).getBytes())); // Fill eviction queue
+            logic.shouldDelegate(client1, Inode.forFile(("file" + (i + 10)).getBytes())); // Fill active queue
+        }
+
+        assertTrue("File should be in eviction queue", logic.isInEvictionQueue(client1, inode1));
+    }
+
+    @Test
+    public void shouldDiscardExpiredEntries() {
+        Inode inode1 = Inode.forFile("file".getBytes());
+
+        logic.shouldDelegate(client1, inode1); // Add to eviction queue
+        logic.shouldDelegate(client1, inode1); // Add to active queue
+
+        clock.advance(Duration.ofSeconds(11));
+
+        // trigger eviction from active queue
+        for (int i = 0; i < ACTIVE_QUEUE_CAPACITY; i++) {
+            // call twice to trigger move to active queue
+            logic.shouldDelegate(client1, Inode.forFile(("file" + (i + 10)).getBytes())); // Fill eviction queue
+            logic.shouldDelegate(client1, Inode.forFile(("file" + (i + 10)).getBytes())); // Fill active queue
+        }
+
+        assertFalse("Expired entry should not be in active queue", logic.isInActive(client1, inode1));
+        assertFalse("Expired entry should not be in eviction queue", logic.isInEvictionQueue(client1, inode1));
+
+    }
+
+
+    @Test
+    public void shouldMoveToEvictionQueueIfIdle() {
+        Inode inode1 = Inode.forFile("file".getBytes());
+
+        logic.shouldDelegate(client1, inode1); // Add to eviction queue
+        logic.shouldDelegate(client1, inode1); // Add to active queue
+
+        clock.advance(Duration.ofSeconds(11));
+
+        logic.shouldDelegate(client1, inode1); // Move to eviction queue as being idle
+
+        assertTrue("File should be in eviction queue", logic.isInEvictionQueue(client1, inode1));
+    }
+
+
+    @Test
+    public void differentClietnsShouldNotAffectEachother() {
+        Inode inode1 = Inode.forFile("file".getBytes());
+
+        logic.shouldDelegate(client1, inode1);
+        logic.shouldDelegate(client2, inode1);
+
+        assertFalse("File should be in eviction queue", logic.isInActive(client1, inode1));
+        assertFalse("File should be in eviction queue", logic.isInActive(client1, inode1));
+    }
+
+    @Test
+    public void clearRemovesAllEntriesFromQueues() {
+        Inode inode = Inode.forFile("file14".getBytes());
+        logic.shouldDelegate(client1, inode); // Add to eviction queue
+        logic.reset();
+        assertFalse("Active queue should be empty after clear", logic.isInActive(client1, inode));
+        assertFalse("Eviction queue should be empty after clear", logic.isInEvictionQueue(client1, inode));
+    }
+
+}
