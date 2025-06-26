d3a5bcea9ca3d0641121b77cc54dc2bd18a8c8c5
Christian Kohlschütter <christian@kohlschutter.com>
2025-06-26 20:23:58 +0200

    dlm: Make DistributedLockManager use AbstractLockManager2
    DistributedLockManager used to create new Base64-strings upon
    objIdToKey(byte[]), which is suboptimal.
    
    Use Opaque.getBase64 instead.
    
    Related: https://github.com/dCache/nfs4j/discussions/149
    Signed-off-by: Christian Kohlschütter <christian@kohlschutter.com>

--- dlm/src/main/java/org/dcache/nfs/v4/nlm/DistributedLockManager.java
+++ dlm/src/main/java/org/dcache/nfs/v4/nlm/DistributedLockManager.java
@@ -25,6 +25,8 @@ import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 
+import org.dcache.nfs.util.Opaque;
+
 import com.hazelcast.core.HazelcastInstance;
 import com.hazelcast.multimap.MultiMap;
 
@@ -45,7 +47,7 @@ import com.hazelcast.multimap.MultiMap;
  *
  * @since 0.16
  */
-public class DistributedLockManager extends AbstractLockManager {
+public class DistributedLockManager extends AbstractLockManager2 {
 
     private final MultiMap<String, NlmLock> locks;
 
@@ -61,7 +63,7 @@ public class DistributedLockManager extends AbstractLockManager {
     }
 
     @Override
-    protected Lock getObjectLock(byte[] objId) {
+    protected Lock getObjectLock(Opaque objId) {
         String key = objIdToKey(objId);
         return new Lock() {
             @Override
@@ -103,40 +105,37 @@ public class DistributedLockManager extends AbstractLockManager {
      * @return collection of active locks.
      */
     @Override
-    protected Collection<NlmLock> getActiveLocks(byte[] objId) {
+    protected Collection<NlmLock> getActiveLocks(Opaque objId) {
         String key = objIdToKey(objId);
         return locks.get(key);
     }
 
     @Override
-    protected void add(byte[] objId, NlmLock lock) {
+    protected void add(Opaque objId, NlmLock lock) {
         String key = objIdToKey(objId);
         locks.put(key, lock);
     }
 
     @Override
-    protected boolean remove(byte[] objId, NlmLock lock) {
+    protected boolean remove(Opaque objId, NlmLock lock) {
         String key = objIdToKey(objId);
         return locks.remove(key, lock);
     }
 
     @Override
-    protected void addAll(byte[] objId, Collection<NlmLock> locks) {
+    protected void addAll(Opaque objId, Collection<NlmLock> locks) {
         String key = objIdToKey(objId);
         locks.forEach(l -> this.locks.put(key, l));
     }
 
     @Override
-    protected void removeAll(byte[] objId, Collection<NlmLock> locks) {
+    protected void removeAll(Opaque objId, Collection<NlmLock> locks) {
         String key = objIdToKey(objId);
         locks.forEach(l -> this.locks.remove(key, l));
     }
 
-    private static String objIdToKey(byte[] objId) {
-        return Base64
-                .getEncoder()
-                .withoutPadding()
-                .encodeToString(objId);
+    private static String objIdToKey(Opaque objId) {
+      return objId.getBase64();
     }
 
 }
