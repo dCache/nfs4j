dCache/nfs4j

commit 09e8063234e4b649d3e748474d8d425f53ce64b8

core: PseudoFS: Properly resolve the inner root inode
When we try to resolve the inner FS's root inode given a pseudo inode in
PseudoFS, we can directly return the inner FS's root inode as returned
by VirtualFileSystem#getRootInode(), which is most likely cached,
instead of creating new Inode instances all over again.

This further reduces the number of Inode allocations.

Also fail with BadHandleException for any other pseudo inode because
they will by definition be invalid in the underlying file system.

Related: https://github.com/dCache/nfs4j/discussions/149
Signed-off-by: Christian Kohlschütter <christian@kohlschutter.com>

core/src/main/java/org/dcache/nfs/vfs/PseudoFs.java
@@ -746,8 +746,17 @@ public class PseudoFs extends ForwardingFileSystem {
      *
      * @param inode The {@link Inode} as passed from and to the NFS client.
      * @return The {@link Inode} as passed from {@link PseudoFs} to the underlying {@link VirtualFileSystem}.
+     * @throws IOException on error.
      */
-    private Inode innerInode(Inode inode) {
+    private Inode innerInode(Inode inode) throws IOException {
+        if (inode.isPseudoInode()) {
+            Inode innerRootInode = _inner.getRootInode();
+            if (innerRootInode.getFileIdKey().equals(inode.getFileIdKey())) {
+                return innerRootInode;
+            } else {
+                throw new BadHandleException();
+            }
+        }
         return Inode.innerInode(inode);
     }
 }

commit d52daa421b0911255efa86951e41b7709d87aa17

core: Fix regression in handling of export table
As reported in #156, commit 09e8063234e4b649d3e748474d8d425f53ce64b8
caused a regression a during dCache integration.

The change threw a BadHandleException if the pseudo Inode was not the
root inode of the inner file system.

Do not throw a BadHandleException but simply convert the pseudo inode to
an inner inode.

Signed-off-by: Christian Kohlschütter <christian@kohlschutter.com>

core/src/main/java/org/dcache/nfs/vfs/PseudoFs.java
@@ -753,8 +753,6 @@ public class PseudoFs extends ForwardingFileSystem {
             Inode innerRootInode = _inner.getRootInode();
             if (innerRootInode.getFileIdKey().equals(inode.getFileIdKey())) {
                 return innerRootInode;
-            } else {
-                throw new BadHandleException();
             }
         }
         return Inode.innerInode(inode);