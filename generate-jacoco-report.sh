320542d8006ac9d5f577a08d3f06ad85de783e3e
chubnastia <ch.nastia20@gmail.com>
2026-01-14 14:03:05 +0100

    removed any hardcoded jacoco ver

--- generate-jacoco-report.sh
+++ generate-jacoco-report.sh
@@ -2,6 +2,7 @@
 
 # Define paths
 PROJECT_ROOT="${PROJECT_ROOT:-$(pwd)}"
+JACOCO_VERSION="${JACOCO_VERSION:-0.8.14}"  #fallback to 0.8.14 if env var is unset
 JACOCO_DIR="${PROJECT_ROOT}/jacoco-$JACOCO_VERSION"
 JACOCO_CLI_JAR="$JACOCO_DIR/lib/jacococli.jar"
 MERGED_EXEC="$PROJECT_ROOT/target/coverage-reports/merged.exec"
